e30e5a9dd3d858b4fbef8ca32517287690b3bbf8
Michael Jalkio <mjalkio@gmail.com>
2017-12-04 21:12:40 -0800

    add test for day2part2

---
+++ day02/day2part2.py
@@ -0,0 +1,12 @@
+def calculate_checksum_divisible(puzzle_input):
+    checksum = 0
+    puzzle_input = puzzle_input.split('\n')
+    for line in puzzle_input:
+        line = line.split()
+        if len(line) == 0:
+            continue
+        line = [element for element in line if len(element) > 0]
+        line = [int(num) for num in line]
+        checksum += max(line) - min(line)
+
+    return checksum
--- day02/test_day2.py
+++ day02/test_day2.py
@@ -1,4 +1,5 @@
 from .day2part1 import calculate_checksum
+from .day2part2 import calculate_checksum_divisible
 
 
 def test_part1():
@@ -9,3 +10,13 @@ def test_part1():
     """
 
     assert calculate_checksum(puzzle_input) == 18
+
+
+def test_part2():
+    puzzle_input = """
+        5 9 2 8
+        9 4 7 3
+        3 8 6 5
+    """
+
+    assert calculate_checksum_divisible(puzzle_input) == 9
