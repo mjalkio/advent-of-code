0310114926689878cd3a424cbcf3efc59b2a6a75
Michael Jalkio <mjalkio@gmail.com>
2017-12-04 21:08:38 -0800

    add day 2 part 1 solution

--- day02/day2part1.py
+++ day02/day2part1.py
@@ -1,2 +1,12 @@
 def calculate_checksum(puzzle_input):
-    return 0
+    checksum = 0
+    puzzle_input = puzzle_input.split('\n')
+    for line in puzzle_input:
+        line = line.split()
+        if len(line) == 0:
+            continue
+        line = [element for element in line if len(element) > 0]
+        line = [int(num) for num in line]
+        checksum += max(line) - min(line)
+
+    return checksum
