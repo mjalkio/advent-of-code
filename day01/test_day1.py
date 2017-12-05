82afece1ea4b6ced711c60c63d42b933401bdbcd
Michael Jalkio <mjalkio@gmail.com>
2017-12-04 20:53:46 -0800

    add test cases for day 1 part 2

--- day01/test_day1.py
+++ day01/test_day1.py
@@ -1,4 +1,5 @@
 from .day1part1 import sum_matches
+from .day1part2 import sum_matches2
 
 
 def test_part1():
@@ -11,3 +12,16 @@ def test_part1():
 
     for puzzle_input, expected in test_cases:
         assert sum_matches(puzzle_input) == expected
+
+
+def test_part2():
+    test_cases = [
+        ('1212', 6),
+        ('1221', 0),
+        ('123425', 4),
+        ('123123', 12),
+        ('12131415', 4),
+    ]
+
+    for puzzle_input, expected in test_cases:
+        assert sum_matches2(puzzle_input) == expected
