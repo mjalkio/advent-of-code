25ad8d4c724d9c1446ab659f65951e6732bef219
Michael Jalkio <mjalkio@gmail.com>
2017-12-04 21:18:36 -0800

    make tests pass for day2part2

--- day02/day2part2.py
+++ day02/day2part2.py
@@ -1,3 +1,13 @@
+def _calculate_line_checksum(line):
+    for i in range(len(line)):
+        for j in range(i + 1, len(line)):
+            if line[i] % line[j] == 0:
+                return line[i] / line[j]
+            elif line[j] % line[i] == 0:
+                return line[j] / line[i]
+    raise Exception("This shouldn't happen!")
+
+
 def calculate_checksum_divisible(puzzle_input):
     checksum = 0
     puzzle_input = puzzle_input.split('\n')
@@ -7,6 +17,6 @@ def calculate_checksum_divisible(puzzle_input):
             continue
         line = [element for element in line if len(element) > 0]
         line = [int(num) for num in line]
-        checksum += max(line) - min(line)
+        checksum += _calculate_line_checksum(line)
 
     return checksum
