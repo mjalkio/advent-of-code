0ff183124790c90e0a41805cfe8e17de80531021
Michael Jalkio <mjalkio@gmail.com>
2017-12-07 07:37:51 -0800

    add solution for day3part1

--- day03/day3part1.py
+++ day03/day3part1.py
@@ -1,2 +1,42 @@
+def data_coordinates(loc):
+    spiral_level = 0
+    side_length = 1
+
+    max_data_value_in_level = 1
+    while loc > max_data_value_in_level:
+        spiral_level += 1
+        side_length = side_length + 2
+        max_data_value_in_level += side_length * 4 - 4
+
+    x = spiral_level
+    y = -spiral_level
+    data_val_at_x_y = max_data_value_in_level
+    if data_val_at_x_y == loc:
+        return (x, y)
+
+    for side in range(4):
+        for _ in range(side_length - 1):
+            if data_val_at_x_y == loc:
+                return (x, y)
+
+            if side == 0:
+                x -= 1
+            elif side == 1:
+                y += 1
+            elif side == 2:
+                x += 1
+            elif side == 3:
+                y -= 1
+
+            data_val_at_x_y -= 1
+
+    raise Exception("That didn't work...")
+
+
+def manhattan_distance(coord1, coord2):
+    return abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
+
+
 def spiral_manhattan_distance(loc):
-    return 0
+    coords = data_coordinates(loc)
+    return manhattan_distance(coords, (0, 0))
--- day03/test_day3.py
+++ day03/test_day3.py
@@ -1,12 +1,28 @@
-from .day3part1 import spiral_manhattan_distance
+from .day3part1 import data_coordinates, spiral_manhattan_distance
+
+
+def test_data_coordinates():
+    test_cases = [
+        (1, (0, 0)),
+        (2, (1, 0)),
+        (12, (2, 1)),
+        (13, (2, 2)),
+        (22, (-1, -2)),
+        (23, (0, -2)),
+        (26, (3, -2)),
+        (35, (-1, 3)),
+    ]
+
+    for data_loc, expected in test_cases:
+        assert data_coordinates(data_loc) == expected
 
 
 def test_part1():
     test_cases = [
-        ('1', 0),
-        ('12', 3),
-        ('23', 2),
-        ('1024', 31),
+        (1, 0),
+        (12, 3),
+        (23, 2),
+        (1024, 31),
     ]
 
     for puzzle_input, expected in test_cases:
