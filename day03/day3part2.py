2faef1389dc5d0049a7e7ac75073444255e345c6
Michael Jalkio <mjalkio@gmail.com>
2017-12-08 07:12:06 -0800

    solve day 3 part 2

--- day03/day3part2.py
+++ day03/day3part2.py
@@ -1,2 +1,48 @@
+def new_spiral_value(x, y, spiral):
+    surrounding = (
+        (x + 1, y),
+        (x + 1, y + 1),
+        (x, y + 1),
+        (x - 1, y + 1),
+        (x - 1, y),
+        (x - 1, y - 1),
+        (x, y - 1),
+        (x + 1, y - 1),
+    )
+
+    return sum([spiral.get(coord, 0) for coord in surrounding])
+
+
 def first_spiral_value_larger(num):
-    return 0
+    spiral = {(0, 0): 1}
+
+    x = 1
+    y = 1
+    side_length = 1
+
+    while True:
+        side_length += 2
+
+        while y < side_length / 2:
+            y += 1
+            spiral[(x, y)] = new_spiral_value(x, y, spiral)
+        while x > -(side_length / 2):
+            x -= 1
+            spiral[(x, y)] = new_spiral_value(x, y, spiral)
+        while y > -(side_length / 2):
+            y -= 1
+            spiral[(x, y)] = new_spiral_value(x, y, spiral)
+        while x < side_length / 2:
+            x += 1
+            spiral[(x, y)] = new_spiral_value(x, y, spiral)
+
+        current_values = spiral.values()
+        current_values.sort()
+        for val in current_values:
+            if val > num:
+                return val
+
+
+if __name__ == '__main__':
+    puzzle_input = 361527
+    print(first_spiral_value_larger(puzzle_input))
