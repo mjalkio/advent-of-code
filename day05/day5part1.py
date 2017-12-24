805227c7e467ece14944a5ebc5620479f29aa143
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 12:12:31 -0500

    add num_jumps function

--- day05/day5part1.py
+++ day05/day5part1.py
@@ -1,2 +1,9 @@
 def num_jumps(instructions):
-    return 0
+    location = 0
+    num_jumps = 0
+    while location >= 0 and location < len(instructions):
+        num_jumps += 1
+        instructions[location] += 1
+        location += instructions[location] - 1
+
+    return num_jumps
