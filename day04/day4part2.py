3f03efcfd9edde1ffa66053945d4cc2660a7e864
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 11:44:48 -0500

    add anagram passphrase checker

--- day04/day4part2.py
+++ day04/day4part2.py
@@ -1,2 +1,11 @@
+from collections import Counter
+
+
 def is_valid_anagram_passphrase(phrase):
-    return True
+    words = phrase.split()
+    char_count_list = [Counter(word) for word in words]
+
+    # Turn character counts into tuple-tuples so they can be hashed
+    char_count_tuples = [tuple(sorted(c.items())) for c in char_count_list]
+
+    return len(words) == len(set(char_count_tuples))
