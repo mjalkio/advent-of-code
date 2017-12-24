13a7368fb291739454470040e267fb7b234bd8b2
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 17:22:52 -0500

    add num_cycles for day 6 part 1

--- day06/day6part1.py
+++ day06/day6part1.py
@@ -1,2 +1,21 @@
-def num_cycles(memory_banks):
-    return 0
+def num_cycles(config):
+    config = tuple(config)
+    past_configs = set()
+    num_cycles = 0
+    while config not in past_configs:
+        past_configs.add(config)
+        num_cycles += 1
+
+        config = list(config)
+        max_index, max_blocks = max(enumerate(config),
+                                    key=lambda (idx, blocks): blocks)
+        config[max_index] = 0
+        config = [blocks + max_blocks / len(config) for blocks in config]
+        remaining_blocks = max_blocks % len(config)
+        idx = max_index + 1
+        while remaining_blocks > 0:
+            config[idx % len(config)] += 1
+            remaining_blocks -= 1
+            idx += 1
+        config = tuple(config)
+    return num_cycles
