c6bb79a2b9630cb8b7264b9838448319be554401
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 11:22:04 -0500

    add test for day 4 part 1

--- day04/test_day4.py
+++ day04/test_day4.py
@@ -1,12 +1,18 @@
-from .day4part1 import is_valid_passphrase
+from .day4part1 import is_valid_passphrase, num_valid_passphrases
+
+PART1_TEST_CASES = (
+    ('aa bb cc dd ee', True),
+    ('aa bb cc dd aa', False),
+    ('aa bb cc dd aaa', True),
+)
 
 
 def test_is_valid_passphrase():
-    test_cases = [
-        ('aa bb cc dd ee', True),
-        ('aa bb cc dd aa', False),
-        ('aa bb cc dd aaa', True),
-    ]
-
-    for passphrase, expected in test_cases:
+    for passphrase, expected in PART1_TEST_CASES:
         assert is_valid_passphrase(passphrase) == expected
+
+
+def test_part1():
+    passphrases = '\n'.join([test_case[0] for test_case in PART1_TEST_CASES])
+    num_valid = sum([test_case[1] for test_case in PART1_TEST_CASES])
+    assert num_valid_passphrases(passphrases) == num_valid
