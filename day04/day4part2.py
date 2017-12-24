0c3da65e986a9a7504fb32dbc2d1828b2da4dd62
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 11:31:21 -0500

    add day 4 part 2 test

---
+++ day04/day4part2.py
@@ -0,0 +1,2 @@
+def is_valid_anagram_passphrase(phrase):
+    return True
--- day04/test_day4.py
+++ day04/test_day4.py
@@ -1,9 +1,18 @@
 from .day4part1 import is_valid_passphrase, num_valid_passphrases
+from .day4part2 import is_valid_anagram_passphrase
 
 PART1_TEST_CASES = (
     ('aa bb cc dd ee', True),
     ('aa bb cc dd aa', False),
     ('aa bb cc dd aaa', True),
+)
+
+PART2_TEST_CASES = (
+    ('abcde fghij', True),
+    ('abcde xyz ecdab', False),
+    ('a ab abc abd abf abj', True),
+    ('iiii oiii ooii oooi oooo', True),
+    ('oiii ioii iioi iiio', False),
 )
 
 
@@ -16,3 +25,8 @@ def test_part1():
     passphrases = '\n'.join([test_case[0] for test_case in PART1_TEST_CASES])
     num_valid = sum([test_case[1] for test_case in PART1_TEST_CASES])
     assert num_valid_passphrases(passphrases) == num_valid
+
+
+def test_is_valid_anagram_passphrase():
+    for phrase, expected in PART2_TEST_CASES:
+        assert is_valid_anagram_passphrase(phrase) == expected
