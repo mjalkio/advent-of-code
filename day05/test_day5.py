05bd74e840ee13d5105f6544aed9600c8cdafad6
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 12:26:01 -0500

    use pytest correctly

--- day05/test_day5.py
+++ day05/test_day5.py
@@ -1,12 +1,17 @@
+import pytest
+
 from day5part1 import num_jumps
 from day5part2 import num_jumps_again
 
-INSTRUCTIONS = [0, 3, 0, 1, -3]
+
+@pytest.fixture
+def instructions():
+    return [0, 3, 0, 1, -3]
 
 
-def test_num_jumps():
-    assert num_jumps(INSTRUCTIONS) == 5
+def test_num_jumps(instructions):
+    assert num_jumps(instructions) == 5
 
 
-def test_num_jumps_again():
-    assert num_jumps_again(INSTRUCTIONS) == 10
+def test_num_jumps_again(instructions):
+    assert num_jumps_again(instructions) == 10
