9f6101e1f56c7440b062de9e0e21852b47566910
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 17:05:28 -0500

    fix test

--- day06/test_day6.py
+++ day06/test_day6.py
@@ -4,9 +4,9 @@ from day6part1 import num_cycles
 
 
 @pytest.fixture
-def memory_banks():
-    return [0, 3, 0, 1, -3]
+def initial_config():
+    return [0, 2, 7, 0]
 
 
-def test_num_cycles(memory_banks):
-    assert num_cycles(memory_banks) == 5
+def test_num_cycles(initial_config):
+    assert num_cycles(initial_config) == 5
