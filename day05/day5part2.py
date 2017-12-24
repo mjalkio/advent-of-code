efba8108771a7b60d91af96ff1618624ec2d42ed
Michael Jalkio <mjalkio@gmail.com>
2017-12-24 12:35:56 -0500

    add day 5 part 2 solution

--- day05/day5part2.py
+++ day05/day5part2.py
@@ -1,2 +1,20 @@
+from day5part1 import get_instructions
+
+
 def num_jumps_again(instructions):
-    return 0
+    location = 0
+    num_jumps = 0
+    while location >= 0 and location < len(instructions):
+        num_jumps += 1
+        new_location = location + instructions[location]
+        if instructions[location] >= 3:
+            instructions[location] -= 1
+        else:
+            instructions[location] += 1
+        location = new_location
+
+    return num_jumps
+
+if __name__ == '__main__':
+    instructions = get_instructions()
+    print(num_jumps_again(instructions))
