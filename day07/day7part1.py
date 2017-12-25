5cd2b661c04ffdc90cef00b1b9f1d5aacf4bb9a6
Michael Jalkio <mjalkio@gmail.com>
2017-12-25 10:49:18 -0500

    attempt to solve day 7 part 1

--- day07/day7part1.py
+++ day07/day7part1.py
@@ -1,4 +1,11 @@
+import os.path as op
 from itertools import chain
+
+
+def get_input(filename):
+    with open(op.join(op.dirname(__file__), filename), 'r') as f:
+        puzzle_input = f.read()
+    return puzzle_input
 
 
 def flatten(list_of_lists):
@@ -14,3 +21,7 @@ def get_root(puzzle_input):
     root = tuple(set(programs) - set(child_programs))
     assert len(root) == 1, 'Should only have a single root'
     return root[0]
+
+if __name__ == '__main__':
+    puzzle_input = get_input('puzzle_input.txt')
+    print(get_root(puzzle_input))
--- day07/test_day7.py
+++ day07/test_day7.py
@@ -1,12 +1,4 @@
-import os.path as op
-
-from day7part1 import get_root
-
-
-def get_input(filename):
-    with open(op.join(op.dirname(__file__), filename), 'r') as f:
-        puzzle_input = f.read()
-    return puzzle_input
+from day7part1 import get_input, get_root
 
 
 def test_get_root():
