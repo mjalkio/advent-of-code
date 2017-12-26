e104aeb2d768d839812f3ffdad8a769082dc9cff
Michael Jalkio <mjalkio@gmail.com>
2017-12-25 19:39:41 -0500

    make test pass for knot_hash

--- day10/day10.py
+++ day10/day10.py
@@ -1,2 +1,21 @@
+def reverse_list(lst, start, length):
+    idx = start % len(lst)
+    for i in reversed(range(length)):
+        temp = lst[idx]
+        swap_idx = (idx + i) % len(lst)
+        lst[idx] = lst[swap_idx]
+        lst[swap_idx] = temp
+        idx = (idx + 1) % len(lst)
+    return lst
+
+
 def knot_hash(lengths, num_list):
-    return 0
+    current_position = 0
+    skip_size = 0
+    for length in lengths:
+        num_list = reverse_list(lst=num_list,
+                                start=current_position,
+                                length=length)
+        current_position += length + skip_size
+        skip_size += 1
+    return num_list[0] * num_list[1]
