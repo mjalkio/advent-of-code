a3d8429458c92fe49315231bc7af795f6672e820
Michael Jalkio <mjalkio@gmail.com>
2017-12-25 20:13:00 -0500

    fix bug and add solution

--- day10/day10.py
+++ day10/day10.py
@@ -1,11 +1,11 @@
 def reverse_list(lst, start, length):
-    idx = start % len(lst)
-    for i in reversed(range(length)):
-        temp = lst[idx]
-        swap_idx = (idx + i) % len(lst)
-        lst[idx] = lst[swap_idx]
-        lst[swap_idx] = temp
-        idx = (idx + 1) % len(lst)
+    for i in range(length / 2):
+        idx1 = (start + i) % len(lst)
+        idx2 = (start + length - 1 - i) % len(lst)
+
+        temp = lst[idx1]
+        lst[idx1] = lst[idx2]
+        lst[idx2] = temp
     return lst
 
 
@@ -16,6 +16,12 @@ def knot_hash(lengths, num_list):
         num_list = reverse_list(lst=num_list,
                                 start=current_position,
                                 length=length)
-        current_position += length + skip_size
+        current_position += (length + skip_size) % len(num_list)
         skip_size += 1
     return num_list[0] * num_list[1]
+
+if __name__ == '__main__':
+    puzzle_input = '197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63'
+    lengths = [int(length) for length in puzzle_input.split(',')]
+    num_list = range(256)
+    print(knot_hash(lengths=lengths, num_list=num_list))
