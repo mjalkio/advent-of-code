4aaa015744994b88dd96b996560640fb91b62614
Michael Jalkio <mjalkio@gmail.com>
2020-12-01 12:30:05 -0800

    Solve puzzle 1

--- 2020/puzzle01/expense_report.py
+++ 2020/puzzle01/expense_report.py
@@ -3,6 +3,11 @@ from pathlib import Path
 
 def sum_to_2020_product(expense_report_entries):
     """Find the product of the two entries in the expense report that sum to 2020."""
+    for i, entry in enumerate(expense_report_entries):
+        for j in range(i + 1, len(expense_report_entries)):
+            if entry + expense_report_entries[j] == 2020:
+                return entry * expense_report_entries[j]
+
     return None
 
 
