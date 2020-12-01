127617eba551eb39673ef8d1f9dcfcc88e98bb7f
Michael Jalkio <mjalkio@gmail.com>
2020-12-01 15:24:06 -0800

    Solve day 1 part 2

--- year_2020/day01/expense_report.py
+++ year_2020/day01/expense_report.py
@@ -1,3 +1,4 @@
+from math import prod
 from pathlib import Path
 
 
@@ -13,6 +14,16 @@ def sum_two_to_2020_product(expense_report_entries):
 
 def sum_three_to_2020_product(expense_report_entries):
     """Find the product of the three entries in the expense report that sum to 2020."""
+    for i in range(len(expense_report_entries)):
+        for j in range(i + 1, len(expense_report_entries)):
+            for k in range(j + 1, len(expense_report_entries)):
+                entries = (
+                    expense_report_entries[i],
+                    expense_report_entries[j],
+                    expense_report_entries[k]
+                )
+                if sum(entries) == 2020:
+                    return prod(entries)
     return None
 
 
@@ -22,4 +33,5 @@ if __name__ == '__main__':
         puzzle_input = f.read()
 
     expense_report_entries = [int(entry) for entry in puzzle_input.split('\n') if entry != '']
-    print(sum_two_to_2020_product(expense_report_entries))
+    print(f"Part 1: {sum_two_to_2020_product(expense_report_entries)}")
+    print(f"Part 2: {sum_three_to_2020_product(expense_report_entries)}")
