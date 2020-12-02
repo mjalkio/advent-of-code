4064697debd074abc47c536da435bb4aa31c2838
Michael Jalkio <mjalkio@gmail.com>
2020-12-02 05:33:09 -0800

    Solve day 2 part 1

--- year_2020/day02/password_philosophy.py
+++ year_2020/day02/password_philosophy.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from pathlib import Path
 
 
 def is_valid_password(character, min_usages, max_usages, password):
@@ -13,4 +14,26 @@ def is_valid_password(character, min_usages, max_usages, password):
 
 
 def num_valid_passwords(puzzle_input):
-    return None
+    lines = [
+        line
+        for line
+        in puzzle_input.split('\n')
+        if line != ''
+    ]
+
+    num_valid = 0
+    for line in lines:
+        policy, password = line.split(':')
+        character_range, character = policy.split(' ')
+        min_usages, max_usages = [int(num) for num in character_range.split('-')]
+        if is_valid_password(character, min_usages, max_usages, password):
+            num_valid += 1
+    return num_valid
+
+
+if __name__ == '__main__':
+    puzzle_input_path = Path(Path(__file__).parent, 'part1_input.txt')
+    with puzzle_input_path.open() as f:
+        puzzle_input = f.read()
+
+    print(f"Part 1: {num_valid_passwords(puzzle_input)}")
