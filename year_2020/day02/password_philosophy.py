23d085b5eae4af28f7c31c676210460792cf76f9
Michael Jalkio <mjalkio@gmail.com>
2020-12-02 06:48:54 -0800

    Refactor parsing input

--- year_2020/day02/password_philosophy.py
+++ year_2020/day02/password_philosophy.py
@@ -2,6 +2,28 @@ from collections import Counter
 from pathlib import Path
 
 
+def _parse_puzzle_input(puzzle_input):
+    lines = [
+        line
+        for line
+        in puzzle_input.split('\n')
+        if line != ''
+    ]
+
+    parsed_input = []
+    for line in lines:
+        policy, password = [s.strip() for s in line.split(':')]
+        character_range, character = policy.split(' ')
+        num_1, num_2 = [int(num) for num in character_range.split('-')]
+        parsed_input.append({
+            'character': character,
+            'num_1': num_1,
+            'num_2': num_2,
+            'password': password,
+        })
+    return parsed_input
+
+
 def is_valid_password_part_1(character, min_usages, max_usages, password):
     password_character_counts = Counter(password)
     if (
@@ -14,19 +36,16 @@ def is_valid_password_part_1(character, min_usages, max_usages, password):
 
 
 def num_valid_passwords_part_1(puzzle_input):
-    lines = [
-        line
-        for line
-        in puzzle_input.split('\n')
-        if line != ''
-    ]
+    parsed_input = _parse_puzzle_input(puzzle_input)
 
     num_valid = 0
-    for line in lines:
-        policy, password = line.split(':')
-        character_range, character = policy.split(' ')
-        min_usages, max_usages = [int(num) for num in character_range.split('-')]
-        if is_valid_password_part_1(character, min_usages, max_usages, password):
+    for password_data in parsed_input:
+        if is_valid_password_part_1(
+            character=password_data['character'],
+            min_usages=password_data['num_1'],
+            max_usages=password_data['num_2'],
+            password=password_data['password'],
+        ):
             num_valid += 1
     return num_valid
 
