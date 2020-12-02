d2ebff51e8b4387c4ce243bc9c3e949f9d9374c5
Michael Jalkio <mjalkio@gmail.com>
2020-12-02 05:18:27 -0800

    Add is_valid_password

--- year_2020/day02/password_philosophy.py
+++ year_2020/day02/password_philosophy.py
@@ -1,5 +1,15 @@
+from collections import Counter
+
+
 def is_valid_password(character, min_usages, max_usages, password):
-    return None
+    password_character_counts = Counter(password)
+    if (
+            password_character_counts[character] >= min_usages
+            and password_character_counts[character] <= max_usages
+    ):
+        return True
+
+    return False
 
 
 def num_valid_passwords(puzzle_input):
