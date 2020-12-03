5c67502c468c2ef094739109833014f7d69d6936
Michael Jalkio <mjalkio@gmail.com>
2020-12-03 11:52:09 -0800

    Solve day 3 part 1

--- year_2020/day03/test_day03.py
+++ year_2020/day03/test_day03.py
@@ -16,4 +16,4 @@ TEST_INPUT_1 = """
 
 
 def test_part_1():
-    assert num_trees_hit(map=TEST_INPUT_1, horizontal_change=1, vertical_change=1) == 7
+    assert num_trees_hit(tree_map=TEST_INPUT_1, horizontal_change=3, vertical_change=1) == 7
--- year_2020/day03/toboggan_trajectory.py
+++ year_2020/day03/toboggan_trajectory.py
@@ -1,8 +1,22 @@
 from pathlib import Path
 
+TREE = '#'
 
-def num_trees_hit(map, horizontal_change=1, vertical_change=1):
-    return None
+
+def num_trees_hit(tree_map, horizontal_change=1, vertical_change=1):
+    tree_map_list = [line for line in tree_map.split() if line != '']
+    tree_map_width = len(tree_map_list[0])
+    vertical_location = 0
+    horizontal_location = 0
+
+    num_trees_hit = 0
+    while vertical_location < len(tree_map_list):
+        if tree_map_list[vertical_location][horizontal_location] == TREE:
+            num_trees_hit += 1
+
+        vertical_location += vertical_change
+        horizontal_location = (horizontal_location + horizontal_change) % tree_map_width
+    return num_trees_hit
 
 
 if __name__ == '__main__':
@@ -10,4 +24,4 @@ if __name__ == '__main__':
     with puzzle_input_path.open() as f:
         puzzle_input = f.read()
 
-    print(f"Part 1: {num_trees_hit(puzzle_input)}")
+    print(f"Part 1: {num_trees_hit(tree_map=puzzle_input, horizontal_change=3, vertical_change=1)}")
