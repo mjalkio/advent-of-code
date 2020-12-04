c201dc349272c8c6f9c3eadbe56207ad012914ab
Michael Jalkio <mjalkio@gmail.com>
2020-12-04 12:51:37 -0800

    Add attempt for day 4 part 2

--- year_2020/day04/passport_processing.py
+++ year_2020/day04/passport_processing.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 
@@ -30,7 +31,45 @@ def passport_is_valid(
     if not check_values:
         return True
 
-    return None
+    numeric_field_ranges = {
+        'byr': (1920, 2002),
+        'iyr': (2010, 2020),
+        'eyr': (2020, 2030),
+        'pid': (0, 999999999),
+    }
+    for field, (min_value, max_value) in numeric_field_ranges.items():
+        if not passport_data[field].isnumeric():
+            return False
+
+        numeric_value = int(passport_data[field])
+        if numeric_value < min_value or numeric_value > max_value:
+            return False
+
+    hgt = passport_data['hgt']
+    if 'in' in hgt:
+        min_hgt = 59
+        max_hgt = 76
+        hgt_value, _ = hgt.split('in')
+    elif 'cm' in hgt:
+        min_hgt = 150
+        max_hgt = 193
+        hgt_value, _ = hgt.split('cm')
+    else:
+        return False
+
+    if not hgt_value.isnumeric():
+        return False
+    if int(hgt_value) < min_hgt or int(hgt_value) > max_hgt:
+        return False
+
+    hcl_pattern = re.compile(r'^#[0-9a-f]{6}')
+    if not hcl_pattern.match(passport_data['hcl']):
+        return False
+
+    if passport_data['ecl'] not in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'):
+        return False
+
+    return True
 
 
 def num_valid_passports(passport_batch, check_values=False):
@@ -48,3 +87,5 @@ if __name__ == '__main__':
         puzzle_input = f.read()
 
     print(f"Part 1: {num_valid_passports(passport_batch=puzzle_input)}")
+    # 113 -> too high
+    print(f"Part 2: {num_valid_passports(passport_batch=puzzle_input, check_values=True)}")
