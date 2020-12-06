b6585447c06733a4661f68b8442fd9a240c31788
Michael Jalkio <mjalkio@gmail.com>
2020-12-06 08:05:07 -0800

    Add num_questions_answered_by_group

--- year_2020/day06/custom_customs.py
+++ year_2020/day06/custom_customs.py
@@ -1,8 +1,12 @@
+from collections import Counter
+
 from util import read_puzzle_input
 
 
 def num_questions_answered_by_group(group_input):
-    return None
+    questions_only = group_input.replace(' ', '').replace('\n', '')
+    question_counts = Counter(questions_only)
+    return len(question_counts)
 
 
 def sum_questions_answered_by_group(puzzle_input):
--- year_2020/day06/test_day06.py
+++ year_2020/day06/test_day06.py
@@ -1,4 +1,13 @@
-from year_2020.day06.custom_customs import sum_questions_answered_by_group
+from year_2020.day06.custom_customs import (
+    num_questions_answered_by_group,
+    sum_questions_answered_by_group,
+)
+
+TEST_CASE_1 = """
+abcx
+abcy
+abcz
+"""
 
 TEST_INPUT_1 = """
 abc
@@ -17,6 +26,10 @@ a
 
 b
 """
+
+
+def test_num_questions_answered_by_group():
+    assert num_questions_answered_by_group(TEST_CASE_1) == 6
 
 
 def test_sum_questions_answered_by_group():
