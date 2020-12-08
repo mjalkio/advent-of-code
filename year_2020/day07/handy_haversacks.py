537f843bb8734b5e1fe7c8549d5b42d72a676523
Michael Jalkio <mjalkio@gmail.com>
2020-12-07 18:53:54 -0800

    Add day 7 part 1 outline

---
+++ year_2020/day07/handy_haversacks.py
@@ -0,0 +1,12 @@
+from util import read_puzzle_input
+
+
+def num_bags_colors_can_contain_my_bag(puzzle_input, my_bag='shiny gold'):
+    return None
+
+
+if __name__ == '__main__':
+    puzzle_input = read_puzzle_input()
+
+    print(f"Part 1: {num_bags_colors_can_contain_my_bag(puzzle_input)}")
+    print(f"Part 2: {None}")
