8d7bdcb3e743b7dd6948239908791a74829f911d
Michael Jalkio <mjalkio@gmail.com>
2020-12-07 21:59:15 -0800

    Solve day 8 part 1

--- year_2020/day08/handheld_halting.py
+++ year_2020/day08/handheld_halting.py
@@ -1,8 +1,34 @@
 from util import read_puzzle_input
 
 
+def _parse_instruction(instruction_line):
+    operation, argument = instruction_line.split()
+    return (operation, int(argument[1:]) if argument[0] == '+' else -1 * int(argument[1:]))
+
+
 def get_acc_at_infinite_loop_start(puzzle_input):
-    return None
+    instructions = [
+        _parse_instruction(line)
+        for line
+        in puzzle_input.split('\n')
+        if line != ''
+    ]
+    accumulator = 0
+    curr_instruction = 0
+    visited_instructions = set()
+    while curr_instruction not in visited_instructions:
+        visited_instructions.add(curr_instruction)
+
+        operation, argument = instructions[curr_instruction]
+        if operation == 'jmp':
+            curr_instruction += argument
+            continue
+
+        if operation == 'acc':
+            accumulator += argument
+        curr_instruction += 1
+
+    return accumulator
 
 
 if __name__ == '__main__':
