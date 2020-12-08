b52ebd6e0e0db8568c5229d4dd5142307a06dea5
Michael Jalkio <mjalkio@gmail.com>
2020-12-07 22:18:23 -0800

    Solve day 8 part 2

--- year_2020/day08/handheld_halting.py
+++ year_2020/day08/handheld_halting.py
@@ -20,6 +20,9 @@ def _run_program(instructions):
     curr_instruction = 0
     visited_instructions = set()
     while curr_instruction not in visited_instructions:
+        if curr_instruction >= len(instructions):
+            return True, accumulator
+
         visited_instructions.add(curr_instruction)
 
         operation, argument = instructions[curr_instruction]
@@ -31,20 +34,36 @@ def _run_program(instructions):
             accumulator += argument
         curr_instruction += 1
 
-    return accumulator
+    return False, accumulator
 
 
 def get_acc_at_infinite_loop_start(puzzle_input):
     instructions = _parse_input(puzzle_input)
-    return _run_program(instructions)
+    _, accumulator = _run_program(instructions)
+    return accumulator
 
 
 def get_acc_at_program_termination(puzzle_input):
-    return None
+    instructions = _parse_input(puzzle_input)
+    for i, (operation, argument) in enumerate(instructions):
+        if operation == 'acc':
+            continue
+
+        modified_instructions = list(instructions)
+        if operation == 'jmp':
+            modified_instructions[i] = ('nop', argument)
+        else:
+            modified_instructions[i] = ('jmp', argument)
+        terminated, accumulator = _run_program(modified_instructions)
+
+        if terminated:
+            return accumulator
+
+    raise ValueError('I am Zardoz')
 
 
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_acc_at_infinite_loop_start(puzzle_input)}")
-    print(f"Part 2: {None}")
+    print(f"Part 2: {get_acc_at_program_termination(puzzle_input)}")
