ba4008c555d9fba6d05075936f9776ce36ee4016
Michael Jalkio <mjalkio@gmail.com>
2020-12-07 21:44:37 -0800

    Add day 8 part 1 outline

---
+++ year_2020/day08/handheld_halting.py
@@ -0,0 +1,12 @@
+from util import read_puzzle_input
+
+
+def get_acc_at_infinite_loop_start(puzzle_input):
+    return None
+
+
+if __name__ == '__main__':
+    puzzle_input = read_puzzle_input()
+
+    print(f"Part 1: {get_acc_at_infinite_loop_start(puzzle_input)}")
+    print(f"Part 2: {None}")
---
+++ year_2020/day08/test_day08.py
@@ -0,0 +1,17 @@
+from year_2020.day08.handheld_halting import get_acc_at_infinite_loop_start
+
+TEST_INPUT = """
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+"""
+
+
+def test_get_acc_at_infinite_loop_start():
+    assert get_acc_at_infinite_loop_start(TEST_INPUT) == 5
