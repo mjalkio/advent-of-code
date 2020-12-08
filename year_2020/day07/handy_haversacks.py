2324c5798460eb44f50ee5b536e7e13a19283d27
Michael Jalkio <mjalkio@gmail.com>
2020-12-07 19:39:27 -0800

    Add day 7 part 2 tests

--- year_2020/day07/handy_haversacks.py
+++ year_2020/day07/handy_haversacks.py
@@ -54,8 +54,12 @@ def num_bags_colors_can_contain_my_bag(puzzle_input, my_bag='shiny gold'):
     return num_bags
 
 
+def num_bags_contained_in_my_bag(puzzle_input, my_bag='shiny gold'):
+    return None
+
+
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {num_bags_colors_can_contain_my_bag(puzzle_input)}")
-    print(f"Part 2: {None}")
+    print(f"Part 2: {num_bags_contained_in_my_bag(puzzle_input)}")
--- year_2020/day07/test_day07.py
+++ year_2020/day07/test_day07.py
@@ -1,4 +1,7 @@
-from year_2020.day07.handy_haversacks import num_bags_colors_can_contain_my_bag
+from year_2020.day07.handy_haversacks import (
+    num_bags_colors_can_contain_my_bag,
+    num_bags_contained_in_my_bag,
+)
 
 TEST_INPUT = """
 light red bags contain 1 bright white bag, 2 muted yellow bags.
@@ -12,6 +15,21 @@ faded blue bags contain no other bags.
 dotted black bags contain no other bags.
 """
 
+TEST_INPUT_2 = """
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+"""
+
 
 def test_num_bags_colors_can_contain_my_bag():
     assert num_bags_colors_can_contain_my_bag(puzzle_input=TEST_INPUT) == 4
+
+
+def test_num_bags_contained_in_my_bag():
+    assert num_bags_contained_in_my_bag(puzzle_input=TEST_INPUT) == 32
+    assert num_bags_contained_in_my_bag(puzzle_input=TEST_INPUT_2) == 126
