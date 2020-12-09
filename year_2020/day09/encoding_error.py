6ab49cb2b02675d6f94a8054e25b1c1df2f35f0d
Michael Jalkio <mjalkio@gmail.com>
2020-12-09 11:03:39 -0800

    Add has_pair_that_sums

--- year_2020/day09/encoding_error.py
+++ year_2020/day09/encoding_error.py
@@ -2,7 +2,11 @@ from util import read_puzzle_input
 
 
 def has_pair_that_sums(number, preamble):
-    return None
+    for i in range(len(preamble) - 1):
+        for j in range(i + 1, len(preamble)):
+            if preamble[i] + preamble[j] == number:
+                return True
+    return False
 
 
 def first_invalid_number(puzzle_input, preamble_size=25):
