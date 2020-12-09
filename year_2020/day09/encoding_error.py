4ed88c555cd152416906b9cf0246c5842947f3d7
Michael Jalkio <mjalkio@gmail.com>
2020-12-09 06:07:06 -0800

    Add day 9 part 1 outline

---
+++ year_2020/day09/encoding_error.py
@@ -0,0 +1,16 @@
+from util import read_puzzle_input
+
+
+def has_pair_that_sums(number, preamble):
+    return None
+
+
+def first_invalid_number(puzzle_input, preamble_size=25):
+    return None
+
+
+if __name__ == '__main__':
+    puzzle_input = read_puzzle_input()
+
+    print(f"Part 1: {first_invalid_number(puzzle_input)}")
+    print(f"Part 2: {None}")
---
+++ year_2020/day09/test_day09.py
@@ -0,0 +1,54 @@
+import pytest
+
+from year_2020.day09.encoding_error import (
+    has_pair_that_sums,
+    first_invalid_number,
+)
+
+EXAMPLE_1_PREAMBLE = range(1, 26)
+EXAMPLE_2_PREAMBLE = list(range(1, 20)) + list(range(21, 26)) + [45]
+
+
+TEST_INPUT = """
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+"""
+
+
+@pytest.mark.parametrize(
+    'number,preamble,expected',
+    [
+        (26, EXAMPLE_1_PREAMBLE, True),
+        (49, EXAMPLE_1_PREAMBLE, True),
+        (100, EXAMPLE_1_PREAMBLE, False),
+        (50, EXAMPLE_1_PREAMBLE, False),
+        (26, EXAMPLE_2_PREAMBLE, True),
+        (65, EXAMPLE_2_PREAMBLE, False),
+        (64, EXAMPLE_2_PREAMBLE, True),
+        (66, EXAMPLE_2_PREAMBLE, True),
+    ]
+)
+def test_has_pair_that_sums(number, preamble, expected):
+    assert has_pair_that_sums(number, preamble) == expected
+
+
+def test_first_invalid_number():
+    assert first_invalid_number(TEST_INPUT, preamble_size=5) == 127
