c565ddad16c5de1ed82ff5e89bf54bf6c327adcc
Michael Jalkio <mjalkio@gmail.com>
2020-12-11 09:35:42 -0800

    Add first attempt that does not work

--- year_2020/day11/seating_system.py
+++ year_2020/day11/seating_system.py
@@ -18,8 +18,43 @@ def _get_num_occupied_seats(seat_layout):
     return sum(seat == OCCUPIED_SEAT for seat in seat_layout.values())
 
 
+def _get_num_occupied_seats_around(i, j, seat_layout):
+    num_occupied_seats_around = 0
+    for ii in range(i - 1, i + 2):
+        for jj in range(j - 1, j + 2):
+            if ii == 0 and jj == 0:
+                continue
+
+            if seat_layout.get((i, j)) == OCCUPIED_SEAT:
+                num_occupied_seats_around += 1
+    return num_occupied_seats_around
+
+
+def _get_next_seat_layout(seat_layout):
+    next_seat_layout = {}
+    for (i, j), current_state in seat_layout.items():
+        if (
+            current_state == EMPTY_SEAT
+            and _get_num_occupied_seats_around(i, j, seat_layout) == 0
+        ):
+            next_seat_layout[(i, j)] = OCCUPIED_SEAT
+        elif (
+            current_state == OCCUPIED_SEAT
+            and _get_num_occupied_seats_around(i, j, seat_layout) >= 4
+        ):
+            next_seat_layout[(i, j)] = EMPTY_SEAT
+        else:
+            next_seat_layout[(i, j)] = current_state
+
+    return next_seat_layout
+
+
 def get_num_occupied_seats_at_convergence(puzzle_input):
     seat_layout = _get_seat_layout(puzzle_input)
+    next_seat_layout = _get_next_seat_layout(seat_layout)
+    while seat_layout != next_seat_layout:
+        seat_layout = next_seat_layout
+        next_seat_layout = _get_next_seat_layout(seat_layout)
     return _get_num_occupied_seats(seat_layout)
 
 
