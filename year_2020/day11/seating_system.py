adbba44c4208bcf2d4f6c504deb0152d6245ef8b
Michael Jalkio <mjalkio@gmail.com>
2020-12-11 09:17:09 -0800

    Add _get_seat_layout

--- year_2020/day11/seating_system.py
+++ year_2020/day11/seating_system.py
@@ -1,7 +1,21 @@
 from util import read_puzzle_input
 
+EMPTY_SEAT = 'L'
+FLOOR = '.'
+OCCUPIED_SEAT = '#'
+
+
+def _get_seat_layout(puzzle_input):
+    lines = [line for line in puzzle_input.split('\n') if line != '']
+    seat_layout = {}
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            seat_layout[(i, j)] = lines[i][j]
+    return seat_layout
+
 
 def get_num_occupied_seats_at_convergence(puzzle_input):
+    seat_layout = _get_seat_layout(puzzle_input)
     return None
 
 
