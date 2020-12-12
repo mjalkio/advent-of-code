4724ed89b0fff0a4998f1bca80d6b9f9d7ba480a
Michael Jalkio <mjalkio@gmail.com>
2020-12-12 14:08:50 -0800

    Implement get_navigation_destination

--- year_2020/day12/rain_risk.py
+++ year_2020/day12/rain_risk.py
@@ -1,11 +1,41 @@
 from util import read_puzzle_input
 
+FORWARD = 'F'
+LEFT = 'L'
+RIGHT = 'R'
+NORTH = 'N'
+EAST = 'E'
+SOUTH = 'S'
+WEST = 'W'
+DIRECTIONS = (NORTH, EAST, SOUTH, WEST)
 
-def get_navigation_destination(puzzle_input, starting_x=0, starting_y=0):
-    return None
+
+def get_navigation_destination(puzzle_input, x=0, y=0, initial_direction=EAST):
+    instructions = [line for line in puzzle_input.split('\n') if line != '']
+    current_direction_idx = DIRECTIONS.index(initial_direction)
+    for instruction in instructions:
+        action = instruction[0]
+        value = int(instruction[1:])
+        if action in (LEFT, RIGHT):
+            turn = (-1 if action == LEFT else 1) * value // 90
+            current_direction_idx = (current_direction_idx + turn) % len(DIRECTIONS)
+            continue
+        if action == FORWARD:
+            action = DIRECTIONS[current_direction_idx]
+
+        if action == NORTH:
+            y += value
+        elif action == SOUTH:
+            y -= value
+        elif action == EAST:
+            x += value
+        elif action == WEST:
+            x -= value
+
+    return x, y
 
 
-def get_manhattan_distance_after_navigation(puzzle_input, starting_x=0, starting_y=0):
+def get_manhattan_distance_after_navigation(puzzle_input, x=0, y=0):
     return None
 
 
