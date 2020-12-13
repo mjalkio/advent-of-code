0480f1831d3a00330e1541ae56ce389486f71f22
Michael Jalkio <mjalkio@gmail.com>
2020-12-13 08:16:24 -0800

    Add day 13 part 1 outline

---
+++ year_2020/day13/shuttle_search.py
@@ -0,0 +1,16 @@
+from util import read_puzzle_input
+
+
+def get_earliest_bus_and_wait_time_for_airport(puzzle_input):
+    return None, None
+
+
+def get_bus_id_times_wait_time(puzzle_input):
+    return None
+
+
+if __name__ == '__main__':
+    puzzle_input = read_puzzle_input()
+
+    print(f"Part 1: {get_bus_id_times_wait_time(puzzle_input)}")
+    print(f"Part 2: {None}")
---
+++ year_2020/day13/test_day13.py
@@ -0,0 +1,14 @@
+from year_2020.day13.shuttle_search import (
+    get_bus_id_times_wait_time,
+    get_earliest_bus_and_wait_time_for_airport,
+)
+
+TEST_INPUT = """
+939
+7,13,x,x,59,x,31,19
+"""
+
+
+def test_part_1():
+    assert get_bus_id_times_wait_time(TEST_INPUT) == 295
+    assert get_earliest_bus_and_wait_time_for_airport(TEST_INPUT) == 59, 5
