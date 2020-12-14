c7dd15341536d006cc2bf915fd18dcc80d4f5b38
Michael Jalkio <mjalkio@gmail.com>
2020-12-14 12:53:22 -0800

    Solve day 14 part 2

--- year_2020/day14/docking_data.py
+++ year_2020/day14/docking_data.py
@@ -27,6 +27,29 @@ def get_masked_value(value, mask):
     return int(''.join(masked_value_bits), base=2)
 
 
+def _get_memory_locations_for_masked_address(masked_address):
+    first_bit = masked_address[0]
+    if first_bit in ('1', '0'):
+        memory_locations = [[first_bit]]
+    else:
+        memory_locations = [['1'], ['0']]
+
+    for bit in masked_address[1:]:
+        if bit in ('1', '0'):
+            for memory_location in memory_locations:
+                if memory_location is None:
+                    import pdb; pdb.set_trace()
+                memory_location.append(bit)
+            continue
+
+        new_memory_locations = []
+        for memory_location in memory_locations:
+            new_memory_locations.append(memory_location + ['1'])
+            new_memory_locations.append(memory_location + ['0'])
+        memory_locations = new_memory_locations
+    return [int(''.join(loc), base=2) for loc in memory_locations]
+
+
 def get_memory_sum(puzzle_input, use_version_2=False):
     initialization_program = [line for line in puzzle_input.split('\n') if line != '']
     memory = {}
@@ -38,7 +61,13 @@ def get_memory_sum(puzzle_input, use_version_2=False):
         mem_instruction, value = line.split(' = ')
         # Instructions are of the form mem[XXX]
         mem_location = int(mem_instruction[4:-1])
-        memory[mem_location] = get_masked_value(value=int(value), mask=mask)
+        if use_version_2:
+            masked_address = get_masked_address(value=mem_location, mask=mask)
+            mem_locations_to_update = _get_memory_locations_for_masked_address(masked_address)
+            for ml in mem_locations_to_update:
+                memory[ml] = int(value)
+        else:
+            memory[mem_location] = get_masked_value(value=int(value), mask=mask)
     return sum(memory.values())
 
 
