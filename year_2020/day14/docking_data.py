22708c7d75a813b43e0e2c1125c85faba62e2a21
Michael Jalkio <mjalkio@gmail.com>
2020-12-14 12:16:51 -0800

    Implement get_masked_value

--- year_2020/day14/docking_data.py
+++ year_2020/day14/docking_data.py
@@ -2,7 +2,14 @@ from util import read_puzzle_input
 
 
 def get_masked_value(value, mask):
-    return None
+    binary_value = bin(value)[2:].zfill(len(mask))
+    masked_value_bits = []
+    for i in range(len(mask)):
+        if mask[i] == 'X':
+            masked_value_bits.append(binary_value[i])
+        else:
+            masked_value_bits.append(mask[i])
+    return int(''.join(masked_value_bits), base=2)
 
 
 def get_memory_sum(puzzle_input):
