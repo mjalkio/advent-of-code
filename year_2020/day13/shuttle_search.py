3e563aa653cc965f8775be9028f56a490f6022ea
Michael Jalkio <mjalkio@gmail.com>
2020-12-13 18:47:27 -0800

    Add day 13 part 2 tests

--- year_2020/day13/shuttle_search.py
+++ year_2020/day13/shuttle_search.py
@@ -19,8 +19,12 @@ def get_bus_id_times_wait_time(puzzle_input):
     return math.prod(get_earliest_bus_and_wait_time_for_airport(puzzle_input))
 
 
+def get_shuttle_company_solution(puzzle_input):
+    return None
+
+
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_bus_id_times_wait_time(puzzle_input)}")
-    print(f"Part 2: {None}")
+    print(f"Part 2: {get_shuttle_company_solution(puzzle_input)}")
--- year_2020/day13/test_day13.py
+++ year_2020/day13/test_day13.py
@@ -1,6 +1,9 @@
+import pytest
+
 from year_2020.day13.shuttle_search import (
     get_bus_id_times_wait_time,
     get_earliest_bus_and_wait_time_for_airport,
+    get_shuttle_company_solution,
 )
 
 TEST_INPUT = """
@@ -8,7 +11,40 @@ TEST_INPUT = """
 7,13,x,x,59,x,31,19
 """
 
+TEST_INPUT_2 = """
+0
+17,x,13,19
+"""
+
+TEST_INPUT_3 = """
+67,7,59,61
+"""
+
+TEST_INPUT_4 = """
+67,x,7,59,61
+"""
+
+TEST_INPUT_5 = """
+67,7,x,59,61
+"""
+
+TEST_INPUT_6 = """
+1789,37,47,1889
+"""
+
 
 def test_part_1():
     assert get_bus_id_times_wait_time(TEST_INPUT) == 295
     assert get_earliest_bus_and_wait_time_for_airport(TEST_INPUT) == (59, 5)
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    (TEST_INPUT, 1068781),
+    (TEST_INPUT_2, 3417),
+    (TEST_INPUT_3, 754018),
+    (TEST_INPUT_4, 779210),
+    (TEST_INPUT_5, 1261476),
+    (TEST_INPUT_6, 1202161486),
+])
+def test_part_2(test_input, expected):
+    assert get_shuttle_company_solution(test_input) == expected
