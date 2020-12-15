e07741de30fb2da049f89e093fde145c6c058a1c
Michael Jalkio <mjalkio@gmail.com>
2020-12-15 08:41:11 -0800

    Solve day 15 part 1

--- year_2020/day15/rambunctious_recitation.py
+++ year_2020/day15/rambunctious_recitation.py
@@ -1,13 +1,23 @@
+from collections import defaultdict
+
 PUZZLE_INPUT = "15,12,0,14,3,1"
 
 
 def get_number_spoken(puzzle_input, num_turns=2020):
     starting_numbers = [int(num) for num in puzzle_input.split(',')]
     last_number_spoken = None
+    times_spoken = defaultdict(list)
     for turn in range(num_turns):
+        times_spoken[last_number_spoken].append(turn)
+
         if turn < len(starting_numbers):
             last_number_spoken = starting_numbers[turn]
-            continue
+        elif len(times_spoken[last_number_spoken]) == 1:
+            last_number_spoken = 0
+        else:
+            last_number_times_spoken = times_spoken[last_number_spoken]
+            last_number_spoken = last_number_times_spoken[-1] - last_number_times_spoken[-2]
+
     return last_number_spoken
 
 
