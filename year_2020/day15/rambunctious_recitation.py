a1dacff2bb3dd53b4db70c71002fd848da1fe456
Michael Jalkio <mjalkio@gmail.com>
2020-12-15 08:34:09 -0800

    Pass handful of test cases

--- year_2020/day15/rambunctious_recitation.py
+++ year_2020/day15/rambunctious_recitation.py
@@ -1,8 +1,14 @@
 PUZZLE_INPUT = "15,12,0,14,3,1"
 
 
-def get_number_spoken(starting_numbers, num_turns=2020):
-    return None
+def get_number_spoken(puzzle_input, num_turns=2020):
+    starting_numbers = [int(num) for num in puzzle_input.split(',')]
+    last_number_spoken = None
+    for turn in range(num_turns):
+        if turn < len(starting_numbers):
+            last_number_spoken = starting_numbers[turn]
+            continue
+    return last_number_spoken
 
 
 if __name__ == '__main__':
