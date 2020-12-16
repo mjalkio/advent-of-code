e108907af289afe0ae694a9b8b7d8203ed4a2fe3
Michael Jalkio <mjalkio@gmail.com>
2020-12-15 22:19:11 -0800

    Pass test for day 16 part 2

--- year_2020/day16/test_day16.py
+++ year_2020/day16/test_day16.py
@@ -18,10 +18,24 @@ nearby tickets:
 38,6,12
 """
 
+TEST_INPUT_2 = """
+class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"""
+
 
 def test_get_ticket_scanning_error_rate():
     assert get_ticket_scanning_error_rate(TEST_INPUT) == 71
 
 
 def test_get_field_order():
-    assert get_field_order(TEST_INPUT) == ['row', 'class', 'seat']
+    assert get_field_order(TEST_INPUT_2) == ['row', 'class', 'seat']
--- year_2020/day16/ticket_translation.py
+++ year_2020/day16/ticket_translation.py
@@ -1,12 +1,12 @@
 from util import read_puzzle_input
 
 
-def _get_ticket_scanning_error_rate(ticket, all_valid_ranges):
-    ticket_scanning_error_rate = 0
+def _get_invalid_field_sum(ticket, all_valid_ranges):
+    invalid_field_sum = 0
     for field_value in ticket:
         if not any(field_value in valid_range for valid_range in all_valid_ranges):
-            ticket_scanning_error_rate += field_value
-    return ticket_scanning_error_rate
+            invalid_field_sum += field_value
+    return invalid_field_sum
 
 
 def _parse_input(puzzle_input):
@@ -43,12 +43,44 @@ def get_ticket_scanning_error_rate(puzzle_input):
 
     ticket_scanning_error_rate = 0
     for ticket in nearby_tickets:
-        ticket_scanning_error_rate += _get_ticket_scanning_error_rate(ticket, all_valid_ranges)
+        ticket_scanning_error_rate += _get_invalid_field_sum(ticket, all_valid_ranges)
     return ticket_scanning_error_rate
 
 
 def get_field_order(puzzle_input):
-    return None
+    rules, my_ticket, nearby_tickets = _parse_input(puzzle_input)
+
+    valid_tickets = [
+        ticket
+        for ticket
+        in nearby_tickets
+        if _get_invalid_field_sum(ticket, _get_all_valid_ranges(rules)) == 0
+    ]
+    valid_tickets.append(my_ticket)
+
+    # Perform process of elimination. Start by saying any position could be any field.
+    # Then go through the tickets and eliminate the fields that don't work.
+    possible_fields = {position: list(rules.keys()) for position in range(len(my_ticket))}
+    for position in range(len(my_ticket)):
+        for ticket in valid_tickets:
+            position_value = ticket[position]
+            for field_name in list(possible_fields[position]):
+                if not any(position_value in valid_range for valid_range in rules[field_name]):
+                    possible_fields[position].remove(field_name)
+
+    # In order for this problem to make sense, we must "know" some fields by now.
+    # Keep eliminating the "known" fields from the other positions where they're included.
+    while any(len(fields) > 1 for fields in possible_fields.values()):
+        known_fields = [fields[0] for fields in possible_fields.values() if len(fields) == 1]
+        for known_field_name in known_fields:
+            for position in possible_fields:
+                if (
+                    len(possible_fields[position]) > 1
+                    and known_field_name in possible_fields[position]
+                ):
+                    possible_fields[position].remove(known_field_name)
+
+    return [possible_fields[position][0] for position in range(len(my_ticket))]
 
 
 def get_departure_sum(puzzle_input):
