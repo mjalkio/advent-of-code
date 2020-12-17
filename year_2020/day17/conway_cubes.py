0a13be28404f66b5976d5d58e2831ba4e0fdb328
Michael Jalkio <mjalkio@gmail.com>
2020-12-17 08:12:48 -0800

    Add print method

--- year_2020/day17/conway_cubes.py
+++ year_2020/day17/conway_cubes.py
@@ -13,6 +13,21 @@ def _get_energy_source(puzzle_input):
     return {0: initial_slice}
 
 
+def print_energy_source(energy_source):
+    z_coordinates = list(energy_source.keys())
+    z_coordinates.sort()
+    for z in z_coordinates:
+        print(f"z={z}")
+        layer = energy_source[z]
+        width = max(x for x, y in layer.keys()) + 1
+        height = max(y for x, y in layer.keys()) + 1
+        for y in range(height):
+            line = []
+            for x in range(width):
+                line.append(layer[(x, y)])
+            print(''.join(line))
+
+
 def get_num_active_cubes(puzzle_input, num_cycles=6):
     energy_source = _get_energy_source(puzzle_input)
     return None
