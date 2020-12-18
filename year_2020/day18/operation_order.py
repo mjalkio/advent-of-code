52afe42e357eb3dec88a2ddfbbc32c273a28067e
Michael Jalkio <mjalkio@gmail.com>
2020-12-18 12:50:33 -0800

    Add parsing of expressions

--- year_2020/day18/operation_order.py
+++ year_2020/day18/operation_order.py
@@ -1,7 +1,59 @@
+import operator
+
 from util import read_puzzle_input
+
+OPERATORS = {
+    '+': operator.add,
+    '*': operator.mul,
+}
+
+
+def _parse_out_parentheses(expression):
+    num_open_parens = 0
+    for i in range(len(expression)):
+        if expression[i] == '(':
+            num_open_parens += 1
+        elif expression[i] == ')':
+            num_open_parens -= 1
+
+        if num_open_parens == 0:
+            return expression[:i + 1].strip(), expression[i + 1:].strip()
+
+
+def _parse_out_number(expression):
+    for i in range(len(expression)):
+        if expression[i] in OPERATORS or expression[i] == '(':
+            return expression[:i].strip(), expression[i:].strip()
+
+
+def _parse_out_operator(expression):
+    for i in range(len(expression)):
+        if expression[i] in OPERATORS:
+            return OPERATORS[expression[i]], expression[i + 1:].strip()
+
+
+def _split_expression(expression):
+    """Split expression and return left_part, operator, right_part, remainder"""
+    if expression.startswith('('):
+        left_part, left_remainder = _parse_out_parentheses(expression)
+    else:
+        left_part, left_remainder = _parse_out_number(expression)
+
+    if left_remainder == '':
+        return left_part, None, None, None
+
+    operator, operator_remainder = _parse_out_operator(left_remainder)
+
+    if operator_remainder.startswith('('):
+        right_part, remainder = _parse_out_parentheses(operator_remainder)
+    else:
+        right_part, remainder = _parse_out_number(operator_remainder)
+
+    return left_part, operator, right_part, remainder
 
 
 def evaluate_expression(expression):
+    left_part, operator, right_part, remainder = _split_expression(expression)
     return None
 
 
