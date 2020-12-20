79921297c218adbbb46601eea3ae6570e993d2cc
Michael Jalkio <mjalkio@gmail.com>
2020-12-20 10:43:51 -0800

    Add _parse_tiles

--- year_2020/day20/jurassic_jigsaw.py
+++ year_2020/day20/jurassic_jigsaw.py
@@ -5,7 +5,13 @@ from util import read_puzzle_input
 
 def _parse_tiles(puzzle_input):
     """Take the input, return a map of ID to tile."""
-    return None
+    tiles = {}
+    tile_definitions = puzzle_input.split('\n\n')
+    for tile_def in tile_definitions:
+        tile_lines = tile_def.split('\n')
+        tile_id = int(tile_lines[0][5:-1])
+        tiles[tile_id] = np.array([list(tile_line) for tile_line in tile_lines[1:]])
+    return tiles
 
 
 def get_corner_ids_product(puzzle_input):
