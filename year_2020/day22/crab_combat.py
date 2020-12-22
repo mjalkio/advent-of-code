ff9883fd7c6edcdd5cb3d101c66ba2c813e5ee0c
Michael Jalkio <mjalkio@gmail.com>
2020-12-22 09:17:09 -0800

    Pass tests

--- year_2020/day22/crab_combat.py
+++ year_2020/day22/crab_combat.py
@@ -3,6 +3,10 @@ from collections import deque
 from util import read_puzzle_input
 
 
+PLAYER_1 = 'P1'
+PLAYER_2 = 'P2'
+
+
 def _get_decks(puzzle_input):
     lines = [line for line in puzzle_input.split('\n') if line != '']
     deck = deque()
@@ -34,15 +38,43 @@ def _get_winner_deck_normal(p1_deck, p2_deck):
     return p1_deck if len(p1_deck) > 0 else p2_deck
 
 
-def _get_winner_deck_recursive(p1_deck, p2_deck):
-    return []
+def _get_winner_and_deck_recursive(p1_deck, p2_deck):
+    previous_states = set()
+    while len(p1_deck) > 0 and len(p2_deck) > 0:
+        current_state = (tuple(p1_deck), tuple(p2_deck))
+        if current_state in previous_states:
+            return p1_deck
+        else:
+            previous_states.add(current_state)
+
+        p1_card = p1_deck.popleft()
+        p2_card = p2_deck.popleft()
+
+        if len(p1_deck) >= p1_card and len(p2_deck) >= p2_card:
+            winner, _ = _get_winner_and_deck_recursive(p1_deck.copy(), p2_deck.copy())
+        elif p1_card > p2_card:
+            winner = PLAYER_1
+        else:
+            winner = PLAYER_2
+
+        if winner == PLAYER_1:
+            p1_deck.append(p1_card)
+            p1_deck.append(p2_card)
+        else:
+            p2_deck.append(p2_card)
+            p2_deck.append(p1_card)
+
+    if len(p1_deck) > 0:
+        return PLAYER_1, p1_deck
+    else:
+        return PLAYER_2, p2_deck
 
 
 def get_winning_combat_score(puzzle_input, recursive=False):
     p1_deck, p2_deck = _get_decks(puzzle_input)
 
     if recursive:
-        winner_deck = _get_winner_deck_recursive(p1_deck, p2_deck)
+        _, winner_deck = _get_winner_and_deck_recursive(p1_deck, p2_deck)
     else:
         winner_deck = _get_winner_deck_normal(p1_deck, p2_deck)
 
