cfe7c568f8e06e7f1e0b36b9f28ceec18b86fda8
Michael Jalkio <mjalkio@gmail.com>
2020-12-23 09:54:32 -0800

    Add day 23 part 1 outline

---
+++ year_2020/day23/crab_cups.py
@@ -0,0 +1,9 @@
+def get_crab_cups(puzzle_input, num_moves=100):
+    return None
+
+
+if __name__ == '__main__':
+    puzzle_input = '792845136'
+
+    print(f"Part 1: {get_crab_cups(puzzle_input)}")
+    print(f"Part 2: {None}")
---
+++ year_2020/day23/test_day23.py
@@ -0,0 +1,10 @@
+import pytest
+
+from year_2020.day23.crab_cups import get_crab_cups
+
+TEST_INPUT = '389125467'
+
+
+@pytest.mark.parametrize('num_moves, expected', [(10, '92658374'), (100, '67384529')])
+def test_get_crap_cups(num_moves, expected):
+    assert get_crab_cups(TEST_INPUT, num_moves=num_moves) == expected
