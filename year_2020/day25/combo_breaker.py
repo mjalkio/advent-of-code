2e61249b646216d9bc68d095229351ac7395c07b
Michael Jalkio <mjalkio@gmail.com>
2020-12-24 23:30:26 -0800

    Solve day 25 part 1

--- year_2020/day25/combo_breaker.py
+++ year_2020/day25/combo_breaker.py
@@ -1,5 +1,25 @@
-def get_encryption_key(public_key_a, public_key_b, public_key_subject_number=7):
-    return None
+PUBLIC_KEY_SUBJECT_NUMBER = 7
+
+
+def _transform_subject_number(value, subject_number):
+    return (value * subject_number) % 20201227
+
+
+def get_loop_size(subject_number, public_key):
+    loop_size = 0
+    value = 1
+    while value != public_key:
+        value = _transform_subject_number(value=value, subject_number=subject_number)
+        loop_size += 1
+    return loop_size
+
+
+def get_encryption_key(public_key_a, public_key_b, public_key_subject_number=PUBLIC_KEY_SUBJECT_NUMBER):
+    loop_size_a = get_loop_size(subject_number=public_key_subject_number, public_key=public_key_a)
+    encryption_key = 1
+    for _ in range(loop_size_a):
+        encryption_key = _transform_subject_number(value=encryption_key, subject_number=public_key_b)
+    return encryption_key
 
 
 if __name__ == '__main__':
--- year_2020/day25/test_day25.py
+++ year_2020/day25/test_day25.py
@@ -1,5 +1,15 @@
-from year_2020.day25.combo_breaker import get_encryption_key
+import pytest
+
+from year_2020.day25.combo_breaker import PUBLIC_KEY_SUBJECT_NUMBER, get_encryption_key, get_loop_size
 
 
 def test_get_encryption_key():
     assert get_encryption_key(5764801, 17807724) == 14897079
+
+
+@pytest.mark.parametrize('public_key, expected', [(17807724, 11), (5764801, 8)])
+def test_get_loop_size(public_key, expected):
+    assert get_loop_size(
+        subject_number=PUBLIC_KEY_SUBJECT_NUMBER,
+        public_key=public_key
+    ) == expected
