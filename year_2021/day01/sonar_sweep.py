4e0e3ead3b18b1e06a83920dbd0da0e2ef3ea031
Michael Jalkio <mjalkio@gmail.com>
2021-12-01 12:58:06 -0800

    Add day 1 part 2 outline

--- year_2021/day01/sonar_sweep.py
+++ year_2021/day01/sonar_sweep.py
@@ -15,7 +15,23 @@ def count_depth_increases(puzzle_input):
             num_increases += 1
     return num_increases
 
+
+def count_sliding_window_depth_increases(puzzle_input):
+    depths = [
+        int(measurement)
+        for measurement
+        in puzzle_input.split('\n')
+        if measurement != ''
+    ]
+
+    num_increases = 0
+    for i in range(1, len(depths)):
+        if depths[i] > depths[i - 1]:
+            num_increases += 1
+    return num_increases
+
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {count_depth_increases(puzzle_input)}")
+    print(f"Part 2: {count_sliding_window_depth_increases(puzzle_input)}")
--- year_2021/day01/test_sonar_sweep.py
+++ year_2021/day01/test_sonar_sweep.py
@@ -1,4 +1,7 @@
-from year_2021.day01.sonar_sweep import count_depth_increases
+from year_2021.day01.sonar_sweep import (
+    count_depth_increases,
+    count_sliding_window_depth_increases,
+)
 
 PUZZLE_INPUT = """
 199
@@ -14,5 +17,9 @@ PUZZLE_INPUT = """
 """
 
 
-def test_sum_to_2020_product():
+def test_count_depth_increases():
     assert count_depth_increases(PUZZLE_INPUT) == 7
+
+
+def test_count_sliding_window_depth_increases():
+    assert count_sliding_window_depth_increases(PUZZLE_INPUT) == 5
