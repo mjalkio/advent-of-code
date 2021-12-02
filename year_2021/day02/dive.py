67f14bda949a469f41f926f76f9e285604876691
Michael Jalkio <mjalkio@gmail.com>
2021-12-02 08:52:26 -0800

    Solve day 2 part 1

--- year_2021/day02/dive.py
+++ year_2021/day02/dive.py
@@ -1,8 +1,33 @@
 from util import read_puzzle_input
 
 
+def _parse_input(puzzle_input):
+    cmd_lines = [
+        line.split()
+        for line
+        in puzzle_input.split('\n')
+        if line != ''
+    ]
+    return [
+        (command, int(distance))
+        for (command, distance)
+        in cmd_lines
+    ]
+
+
 def get_position_product(puzzle_input):
-    return None
+    cmd_lines = _parse_input(puzzle_input)
+
+    position = 0
+    depth = 0
+    for command, distance in cmd_lines:
+        if command == 'forward':
+            position += distance
+        if command == 'down':
+            depth += distance
+        if command == 'up':
+            depth -= distance
+    return position * depth
 
 
 if __name__ == '__main__':
