62baa8a57fb4d11d8d1e1dc659acbef71603b75d
Michael Jalkio <mjalkio@gmail.com>
2021-12-03 15:40:06 -0800

    Solve day 3 part 2

--- year_2021/day03/binary_diagnostic.py
+++ year_2021/day03/binary_diagnostic.py
@@ -24,7 +24,47 @@ def get_power_consumption(puzzle_input):
 
 
 def get_life_support_rating(puzzle_input):
-    return 0
+    lines = puzzle_input.split()
+    oxygen_rating_candidates = list(lines)
+    c02_rating_candidates = list(lines)
+
+    for i in range(len(oxygen_rating_candidates[0])):
+        if len(oxygen_rating_candidates) == 1:
+            break
+
+        oxygen_bits = [num[i] for num in oxygen_rating_candidates]
+        oxygen_counts = Counter(oxygen_bits)
+        if oxygen_counts['1'] >= oxygen_counts['0']:
+            most_common_val = '1'
+        else:
+            most_common_val = '0'
+        oxygen_rating_candidates = [
+            num
+            for num
+            in oxygen_rating_candidates
+            if num[i] == most_common_val
+        ]
+
+    for i in range(len(c02_rating_candidates[0])):
+        if len(c02_rating_candidates) == 1:
+            break
+
+        c02_bits = [num[i] for num in c02_rating_candidates]
+        c02_counts = Counter(c02_bits)
+        if c02_counts['1'] >= c02_counts['0']:
+            least_common_val = '0'
+        else:
+            least_common_val = '1'
+        c02_rating_candidates = [
+            num
+            for num
+            in c02_rating_candidates
+            if num[i] == least_common_val
+        ]
+
+    oxygen_generator_rating = oxygen_rating_candidates[0]
+    c02_scrubber_rating = c02_rating_candidates[0]
+    return int(oxygen_generator_rating, 2) * int(c02_scrubber_rating, 2)
 
 
 if __name__ == '__main__':
