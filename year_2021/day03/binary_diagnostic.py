1dffedf84dc029a1cecaa505054994875641999b
Michael Jalkio <mjalkio@gmail.com>
2021-12-03 15:21:26 -0800

    Solve day 3 part 1

--- year_2021/day03/binary_diagnostic.py
+++ year_2021/day03/binary_diagnostic.py
@@ -1,8 +1,26 @@
+from collections import Counter
+
 from util import read_puzzle_input
 
 
 def get_power_consumption(puzzle_input):
-    return 0
+    lines = puzzle_input.split()
+    bits_by_position = []
+    for i in range(len(lines[0])):
+        bits_by_position.append([num[i] for num in lines])
+
+    gamma_rate = ''
+    epsilon_rate = ''
+    for bits in bits_by_position:
+        counts = Counter(bits)
+        if counts['1'] > counts['0']:
+            gamma_rate += '1'
+            epsilon_rate += '0'
+        else:
+            gamma_rate += '0'
+            epsilon_rate += '1'
+
+    return int(gamma_rate, 2) * int(epsilon_rate, 2)
 
 
 if __name__ == '__main__':
