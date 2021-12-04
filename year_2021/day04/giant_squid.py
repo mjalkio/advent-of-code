f087edca468bb554caa18e36ba1a00fda93290fe
Michael Jalkio <mjalkio@gmail.com>
2021-12-04 09:09:34 -0800

    Solve day 4 part 1

--- year_2021/day04/giant_squid.py
+++ year_2021/day04/giant_squid.py
@@ -1,30 +1,40 @@
+from collections import namedtuple
+
 from util import read_puzzle_input
 
 GRID_SIZE = 5
 
+Board = namedtuple('Board', ['rows', 'cols', 'all_nums'])
 
-def get_winning_board_score(puzzle_input):
-    inputs = puzzle_input.split('\n\n')
-    numbers = [int(num) for num in inputs[0].split(',')]
-    boards = [board.split() for board in inputs[1:]]
 
-    board_possible_wins = []
-    for b in boards:
+def _get_boards(board_inputs):
+    board_strings = [board.split() for board in board_inputs]
+    boards = []
+    for b in board_strings:
         board_numbers = [int(num) for num in b]
         rows = []
         cols = []
         for i in range(GRID_SIZE):
             rows.append(board_numbers[i * GRID_SIZE:i * GRID_SIZE + GRID_SIZE])
             cols.append(board_numbers[i::GRID_SIZE])
-        board_possible_wins.append({
-            'rows': rows,
-            'cols': cols,
-        })
+        boards.append(Board(rows=rows, cols=cols, all_nums=board_numbers))
+    return boards
 
-    for drawn_number in numbers:
-        pass
 
-    return 0
+def _has_win(sequences, drawn_numbers):
+    return any([len(set(seq) - drawn_numbers) == 0 for seq in sequences])
+
+
+def get_winning_board_score(puzzle_input):
+    inputs = puzzle_input.split('\n\n')
+    numbers = [int(num) for num in inputs[0].split(',')]
+    boards = _get_boards(inputs[1:])
+
+    for move_num in range(len(numbers)):
+        drawn_numbers = set(numbers[:move_num + 1])
+        for b in boards:
+            if _has_win(b.rows, drawn_numbers) or _has_win(b.cols, drawn_numbers):
+                return sum(set(b.all_nums) - drawn_numbers) * numbers[move_num]
 
 
 if __name__ == '__main__':
