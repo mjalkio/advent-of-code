4c80476f29f44dc73532a622e3e2052b0282b999
Michael Jalkio <mjalkio@gmail.com>
2021-12-04 21:44:04 -0800

    Solve day 5 part 1

--- year_2021/day05/hydrothermal_venture.py
+++ year_2021/day05/hydrothermal_venture.py
@@ -1,8 +1,27 @@
+from collections import defaultdict
+
 from util import read_puzzle_input
 
 
 def get_num_dangerous_points(puzzle_input):
-    return 0
+    lines = puzzle_input.split('\n')
+
+    line_counts = defaultdict(int)
+    for line in lines:
+        start_pt, end_pt = line.split(' -> ')
+        x1, y1 = [int(coord) for coord in start_pt.split(',')]
+        x2, y2 = [int(coord) for coord in end_pt.split(',')]
+
+        if x1 == x2:
+            start_y, end_y = sorted([y1, y2])
+            for y in range(start_y, end_y + 1):
+                line_counts[(x1, y)] += 1
+        if y1 == y2:
+            start_x, end_x = sorted([x1, x2])
+            for x in range(start_x, end_x + 1):
+                line_counts[(x, y1)] += 1
+
+    return sum([count >= 2 for count in line_counts.values()])
 
 
 if __name__ == '__main__':
