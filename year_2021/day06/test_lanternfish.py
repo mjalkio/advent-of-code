d4354643269d98b8bfc6ef645c6e9a0a9a1dff80
Michael Jalkio <mjalkio@gmail.com>
2021-12-05 23:34:58 -0800

    Add day 6 outline

---
+++ year_2021/day06/test_lanternfish.py
@@ -0,0 +1,9 @@
+from util import read_puzzle_input
+from year_2021.day06.lanternfish import (
+    get_num_lanternfish,
+)
+
+
+def test_get_num_lanternfish():
+    assert get_num_lanternfish(read_puzzle_input('test_input.txt'), num_days=18) == 26
+    assert get_num_lanternfish(read_puzzle_input('test_input.txt'), num_days=80) == 5934
