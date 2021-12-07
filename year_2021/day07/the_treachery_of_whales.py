a2f27cf1880bc0096fbc198c9941094c76766ece
Michael Jalkio <mjalkio@gmail.com>
2021-12-06 23:37:09 -0800

    Speed up day 7 part 2

--- year_2021/day07/the_treachery_of_whales.py
+++ year_2021/day07/the_treachery_of_whales.py
@@ -15,12 +15,17 @@ def get_fuel_spent(puzzle_input):
 
 def get_fuel_spent_part_two(puzzle_input):
     positions = [int(pos) for pos in puzzle_input.split(',')]
+    dist_to_fuel_cache = {}
     least_fuel_spent = None
     for possible_pos in range(min(positions), max(positions) + 1):
         fuel_spent = 0
         for pos in positions:
             distance = abs(pos - possible_pos)
-            fuel_spent += sum(range(distance + 1))
+
+            if distance not in dist_to_fuel_cache:
+                dist_to_fuel_cache[distance] = sum(range(distance + 1))
+
+            fuel_spent += dist_to_fuel_cache[distance]
         if least_fuel_spent is None or fuel_spent < least_fuel_spent:
             least_fuel_spent = fuel_spent
     return least_fuel_spent
