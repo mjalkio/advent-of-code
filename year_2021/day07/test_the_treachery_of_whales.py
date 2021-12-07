4dda41917c963d6e5ca5f6e37d6ca3ba64398b89
Michael Jalkio <mjalkio@gmail.com>
2021-12-06 23:36:51 -0800

    Solve day 7 part 2

--- year_2021/day07/test_the_treachery_of_whales.py
+++ year_2021/day07/test_the_treachery_of_whales.py
@@ -1,8 +1,13 @@
 from util import read_puzzle_input
 from year_2021.day07.the_treachery_of_whales import (
     get_fuel_spent,
+    get_fuel_spent_part_two,
 )
 
 
 def test_get_fuel_spent():
     assert get_fuel_spent(read_puzzle_input('test_input.txt')) == 37
+
+
+def test_get_fuel_spent_part_two():
+    assert get_fuel_spent_part_two(read_puzzle_input('test_input.txt')) == 168
--- year_2021/day07/the_treachery_of_whales.py
+++ year_2021/day07/the_treachery_of_whales.py
@@ -13,8 +13,21 @@ def get_fuel_spent(puzzle_input):
     return least_fuel_spent
 
 
+def get_fuel_spent_part_two(puzzle_input):
+    positions = [int(pos) for pos in puzzle_input.split(',')]
+    least_fuel_spent = None
+    for possible_pos in range(min(positions), max(positions) + 1):
+        fuel_spent = 0
+        for pos in positions:
+            distance = abs(pos - possible_pos)
+            fuel_spent += sum(range(distance + 1))
+        if least_fuel_spent is None or fuel_spent < least_fuel_spent:
+            least_fuel_spent = fuel_spent
+    return least_fuel_spent
+
+
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_fuel_spent(puzzle_input)}")
-    print(f"Part 2: {get_fuel_spent(puzzle_input)}")
+    print(f"Part 2: {get_fuel_spent_part_two(puzzle_input)}")
