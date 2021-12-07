d77bc0fab077c2f3951899948bee2fb708ddd033
Michael Jalkio <mjalkio@gmail.com>
2021-12-06 23:29:55 -0800

    Solve day 7 part 1

--- year_2021/day07/the_treachery_of_whales.py
+++ year_2021/day07/the_treachery_of_whales.py
@@ -2,7 +2,15 @@ from util import read_puzzle_input
 
 
 def get_fuel_spent(puzzle_input):
-    return 0
+    positions = [int(pos) for pos in puzzle_input.split(',')]
+    least_fuel_spent = None
+    for possible_pos in range(min(positions), max(positions) + 1):
+        fuel_spent = 0
+        for pos in positions:
+            fuel_spent += abs(pos - possible_pos)
+        if least_fuel_spent is None or fuel_spent < least_fuel_spent:
+            least_fuel_spent = fuel_spent
+    return least_fuel_spent
 
 
 if __name__ == '__main__':
