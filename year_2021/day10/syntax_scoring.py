e082211ac613bfc0f57e2297fce43e1dda063f60
Michael Jalkio <mjalkio@gmail.com>
2021-12-10 23:21:23 -0800

    Add day 10 incorrect implementation

--- year_2021/day10/syntax_scoring.py
+++ year_2021/day10/syntax_scoring.py
@@ -1,8 +1,37 @@
+from collections import defaultdict
+
 from util import read_puzzle_input
 
 
+CHUNK_MAP = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+    ">": "<",
+}
+CHUNK_SCORES = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+OPEN_CHUNKS = CHUNK_MAP.values()
+
 def get_syntax_error_score(puzzle_input):
-    return 0
+    lines = puzzle_input.split('\n')
+    score = 0
+    for line in lines:
+        open_chunk_counts = defaultdict(int)
+        for ch in line:
+            if ch in OPEN_CHUNKS:
+                open_chunk_counts[ch] += 1
+            elif open_chunk_counts[CHUNK_MAP[ch]] > 0:
+                open_chunk_counts[CHUNK_MAP[ch]] -= 1
+            else:
+                score += CHUNK_SCORES[ch]
+                break
+
+    return score
 
 
 if __name__ == "__main__":
