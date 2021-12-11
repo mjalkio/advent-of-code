892aa74eb30e16c51a401c2e30204a0c9314b25d
Michael Jalkio <mjalkio@gmail.com>
2021-12-10 19:26:19 -0800

    Solve day 8 part 1

--- year_2021/day08/seven_segment_search.py
+++ year_2021/day08/seven_segment_search.py
@@ -2,7 +2,13 @@ from util import read_puzzle_input
 
 
 def get_num_easy_digit_outputs(puzzle_input):
-    return 0
+    entries = puzzle_input.split('\n')
+
+    num_easy_digits = 0
+    for entry in entries:
+        unique_patterns, output_digits = [digits.split(' ') for digits in entry.split(' | ')]
+        num_easy_digits += sum(len(d) in (2, 3, 4, 7) for d in output_digits)
+    return num_easy_digits
 
 
 if __name__ == '__main__':
