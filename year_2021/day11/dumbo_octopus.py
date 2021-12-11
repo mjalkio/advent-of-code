a49c443bfee33834a34bb71f1b7726de10b74ce0
Michael Jalkio <mjalkio@gmail.com>
2021-12-11 00:04:38 -0800

    Solve day 11 part 2

--- year_2021/day11/dumbo_octopus.py
+++ year_2021/day11/dumbo_octopus.py
@@ -39,7 +39,38 @@ def get_num_flashes(puzzle_input, num_days=100):
 
 
 def get_first_step_all_flash(puzzle_input):
-    return 0
+    lines = puzzle_input.split("\n")
+    energy_levels = {}
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            energy_levels[(x, y)] = int(lines[y][x])
+
+    i = 0
+    while True:
+        i += 1
+        flashes = []
+        for x, y in energy_levels.keys():
+            energy_levels[(x, y)] += 1
+            if energy_levels[(x, y)] == FLASHPOINT:
+                flashes.append((x, y))
+
+        while len(flashes) > 0:
+            x, y = flashes.pop()
+            for dx, dy in product(range(-1, 2), range(-1, 2)):
+                coord = (x + dx, y + dy)
+                if coord not in energy_levels:
+                    continue
+
+                energy_levels[coord] += 1
+                if energy_levels[coord] == FLASHPOINT:
+                    flashes.append(coord)
+
+        if all(level >= FLASHPOINT for level in energy_levels.values()):
+            return i
+
+        for x, y in energy_levels.keys():
+            if energy_levels[(x, y)] >= FLASHPOINT:
+                energy_levels[(x, y)] = 0
 
 
 if __name__ == "__main__":
