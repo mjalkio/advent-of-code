2b3807ff4d537de0c8915cab2c2022cd395d9dcb
Michael Jalkio <mjalkio@gmail.com>
2021-12-10 19:28:38 -0800

    Add day 8 part 2 outline

--- year_2021/day08/seven_segment_search.py
+++ year_2021/day08/seven_segment_search.py
@@ -11,8 +11,12 @@ def get_num_easy_digit_outputs(puzzle_input):
     return num_easy_digits
 
 
+def get_outputs_sum(puzzle_input):
+    return 0
+
+
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_num_easy_digit_outputs(puzzle_input)}")
-    print(f"Part 2: {get_num_easy_digit_outputs(puzzle_input)}")
+    print(f"Part 2: {get_outputs_sum(puzzle_input)}")
--- year_2021/day08/test_seven_segment_search.py
+++ year_2021/day08/test_seven_segment_search.py
@@ -1,8 +1,13 @@
 from util import read_puzzle_input
 from year_2021.day08.seven_segment_search import (
     get_num_easy_digit_outputs,
+    get_outputs_sum,
 )
 
 
 def test_get_num_easy_digit_outputs():
     assert get_num_easy_digit_outputs(read_puzzle_input('test_input.txt')) == 26
+
+
+def test_get_outputs_sum():
+    assert get_outputs_sum(read_puzzle_input('test_input.txt')) == 61229
