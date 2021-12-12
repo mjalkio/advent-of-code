73b42c40bf502175320a80df4e56c0b20759973f
Michael Jalkio <mjalkio@gmail.com>
2021-12-12 10:05:27 -0800

    Add incorrect solution to day 12 part 1

--- year_2021/day12/passage_pathing.py
+++ year_2021/day12/passage_pathing.py
@@ -1,8 +1,42 @@
+from copy import copy
+from collections import defaultdict, namedtuple
+
 from util import read_puzzle_input
 
 
+START = "start"
+END = "end"
+
+Path = namedtuple("Path", ["current_cave", "visited_caves"])
+
+
+def _is_big_cave(cave):
+    return cave.isupper()
+
+
 def get_num_paths(puzzle_input):
-    return 0
+    edges = [line.split("-") for line in puzzle_input.split("\n")]
+    outbound_edges = defaultdict(list)
+    for x, y in edges:
+        outbound_edges[x].append(y)
+
+    potential_paths = [Path(current_cave=START, visited_caves=set([START]))]
+    num_paths = 0
+    while len(potential_paths) > 0:
+        current_cave, visited_caves = potential_paths.pop()
+        if current_cave == END:
+            num_paths += 1
+            continue
+
+        for next_cave in outbound_edges[current_cave]:
+            if _is_big_cave(next_cave) or next_cave not in visited_caves:
+                new_visited_caves = copy(visited_caves)
+                new_visited_caves.add(next_cave)
+                potential_paths.append(
+                    Path(current_cave=next_cave, visited_caves=new_visited_caves)
+                )
+
+    return num_paths
 
 
 if __name__ == "__main__":
