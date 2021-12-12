ec25d20c45e3bdd3e167768e2da82e9aef0c9f0f
Michael Jalkio <mjalkio@gmail.com>
2021-12-12 10:25:18 -0800

    Solve day 12 part 1

--- year_2021/day12/passage_pathing.py
+++ year_2021/day12/passage_pathing.py
@@ -1,13 +1,11 @@
 from copy import copy
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 
 from util import read_puzzle_input
 
 
 START = "start"
 END = "end"
-
-Path = namedtuple("Path", ["current_cave", "visited_caves"])
 
 
 def _is_big_cave(cave):
@@ -19,22 +17,22 @@ def get_num_paths(puzzle_input):
     outbound_edges = defaultdict(list)
     for x, y in edges:
         outbound_edges[x].append(y)
+        outbound_edges[y].append(x)
 
-    potential_paths = [Path(current_cave=START, visited_caves=set([START]))]
+    potential_paths = [[START]]
     num_paths = 0
     while len(potential_paths) > 0:
-        current_cave, visited_caves = potential_paths.pop()
+        current_path = potential_paths.pop()
+        current_cave = current_path[-1]
         if current_cave == END:
             num_paths += 1
             continue
 
         for next_cave in outbound_edges[current_cave]:
-            if _is_big_cave(next_cave) or next_cave not in visited_caves:
-                new_visited_caves = copy(visited_caves)
-                new_visited_caves.add(next_cave)
-                potential_paths.append(
-                    Path(current_cave=next_cave, visited_caves=new_visited_caves)
-                )
+            if _is_big_cave(next_cave) or next_cave not in current_path:
+                new_path = copy(current_path)
+                new_path.append(next_cave)
+                potential_paths.append(new_path)
 
     return num_paths
 
