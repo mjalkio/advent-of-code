3e1ef823718c0091393c94e9e53dbd8a47145c64
Michael Jalkio <mjalkio@gmail.com>
2021-12-12 16:01:34 -0800

    Parametrize day 6

--- year_2021/day06/test_lanternfish.py
+++ year_2021/day06/test_lanternfish.py
@@ -1,14 +1,22 @@
+import pytest
+
 from util import read_puzzle_input
 from year_2021.day06.lanternfish import (
     get_num_lanternfish,
 )
 
 
-def test_get_num_lanternfish():
-    assert get_num_lanternfish(read_puzzle_input("test_input.txt"), num_days=0) == 5
-    assert get_num_lanternfish(read_puzzle_input("test_input.txt"), num_days=18) == 26
-    assert get_num_lanternfish(read_puzzle_input("test_input.txt"), num_days=80) == 5934
+@pytest.mark.parametrize(
+    "num_days,expected_output",
+    [
+        (0, 5),
+        (18, 26),
+        (80, 5934),
+        (256, 26984457539),
+    ],
+)
+def test_get_num_lanternfish(num_days, expected_output):
     assert (
-        get_num_lanternfish(read_puzzle_input("test_input.txt"), num_days=256)
-        == 26984457539
+        get_num_lanternfish(read_puzzle_input("test_input.txt"), num_days=num_days)
+        == expected_output
     )
