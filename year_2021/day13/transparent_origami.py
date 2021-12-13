9595aae84d133386b2fb22ecf5c5caa85232dde5
Michael Jalkio <mjalkio@gmail.com>
2021-12-12 21:25:46 -0800

    Solve day 13 part 1

--- year_2021/day13/transparent_origami.py
+++ year_2021/day13/transparent_origami.py
@@ -2,7 +2,28 @@ from util import read_puzzle_input
 
 
 def get_num_dots(puzzle_input):
-    return 0
+    dot_lines, fold_lines = [line.split("\n") for line in puzzle_input.split("\n\n")]
+    dot_map = set()
+    for line in dot_lines:
+        x, y = [int(coord) for coord in line.split(",")]
+        dot_map.add((x, y))
+
+    instruction, line_num = fold_lines[0].split("=")
+    if instruction == "fold along x":
+        fold_idx = 0
+    else:
+        fold_idx = 1
+    line_num = int(line_num)
+    new_dot_map = set()
+    for coord in dot_map:
+        if coord[fold_idx] < line_num:
+            new_dot_map.add(coord)
+            continue
+        distance_from_fold = coord[fold_idx] - line_num
+        mutable_coord = list(coord)
+        mutable_coord[fold_idx] = line_num - distance_from_fold
+        new_dot_map.add(tuple(mutable_coord))
+    return len(new_dot_map)
 
 
 if __name__ == "__main__":
