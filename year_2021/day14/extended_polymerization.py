b38867f665e21614077f3f1578f068df72a8cc65
Michael Jalkio <mjalkio@gmail.com>
2021-12-13 22:20:01 -0800

    Tweak

--- year_2021/day14/extended_polymerization.py
+++ year_2021/day14/extended_polymerization.py
@@ -11,23 +11,28 @@ def _apply_rules(rules, polymer):
         return polymer
 
     split_idx = len(polymer) // 2
+    middle_element = polymer[split_idx]
     left_side = _apply_rules(rules, polymer[:split_idx])
     right_side = _apply_rules(rules, polymer[split_idx + 1 :])
 
-    if left_side[-1] + polymer[split_idx] in rules:
-        left_side = left_side + rules[left_side[-1] + polymer[split_idx]]
+    if left_side[-1] + middle_element in rules:
+        new_left_side = left_side + rules[left_side[-1] + middle_element][1]
 
-    if polymer[split_idx] + right_side[0] in rules:
-        right_side = rules[polymer[split_idx] + right_side[0]] + right_side
+    if middle_element + right_side[0] in rules:
+        new_right_side = rules[middle_element + right_side[0]][1] + right_side
 
-    new_polymer = left_side + polymer[split_idx] + right_side
+    new_polymer = new_left_side + middle_element + new_right_side
     rules[polymer] = new_polymer
     return new_polymer
 
 
 def get_difference_most_least_common_element_counts(puzzle_input, num_steps=10):
-    polymer, rules = puzzle_input.split("\n\n")
-    rules = dict([rule.split(" -> ") for rule in rules.split("\n")])
+    polymer, rule_input = puzzle_input.split("\n\n")
+
+    rules = {}
+    for rule in rule_input.split("\n"):
+        pair, insertion = rule.split(" -> ")
+        rules[pair] = pair[0] + insertion + pair[1]
 
     for _ in range(num_steps):
         polymer = _apply_rules(rules, polymer)
