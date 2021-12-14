0277805302307962da5a5a33f354bf31c0be11de
Michael Jalkio <mjalkio@gmail.com>
2021-12-13 23:37:49 -0800

    Generate outlines for previous years

--- util.py
+++ util.py
@@ -18,7 +18,7 @@ def read_puzzle_input(file_name="puzzle_input.txt"):
     return puzzle_input
 
 
-def setup_day():
+def setup_day(year=""):
     problem_name = input("Enter the name of today's problem: ")
     problem_slug = problem_name.lower().replace(" ", "_")
     method_name = input("Enter the method name you plan to implement: ")
@@ -26,21 +26,32 @@ def setup_day():
 
     repo_dir = Path(__file__).parent
     year_dirs = [
-        file for file in repo_dir.iterdir() if file.is_dir() and "year" in file.name
+        file
+        for file in repo_dir.iterdir()
+        if file.is_dir() and f"year_{year}" in file.name
     ]
-    current_year_dir = sorted(year_dirs)[-1]
+
+    if len(year_dirs) == 0:
+        new_year_dir = Path(repo_dir, f"year_{year}")
+        new_year_dir.mkdir()
+        Path(new_year_dir, "__init__.py").touch()
+
+        year_dirs = [new_year_dir]
+
+    year_dir = sorted(year_dirs)[-1]
 
     day_dirs = [
-        file
-        for file in current_year_dir.iterdir()
-        if file.is_dir() and "day" in file.name
+        file for file in year_dir.iterdir() if file.is_dir() and "day" in file.name
     ]
+
+    if len(day_dirs) == 0:
+        day_dirs = [Path(year_dir, "day00")]
 
     yesterday_dir = sorted(day_dirs)[-1]
     yesterday_day = yesterday_dir.name[3:]
     today_day = int(yesterday_day) + 1
     today_day_string = str(today_day).zfill(2)
-    today_dir = Path(current_year_dir, f"day{today_day_string}")
+    today_dir = Path(year_dir, f"day{today_day_string}")
     today_dir.mkdir()
 
     Path(today_dir, "__init__.py").touch()
@@ -63,7 +74,7 @@ if __name__ == "__main__":
     Path(today_dir, f"{problem_slug}.py").write_text(problem_contents)
 
     test_contents = f"""from util import read_puzzle_input
-from {current_year_dir.name}.day{today_day_string}.{problem_slug} import (
+from {year_dir.name}.day{today_day_string}.{problem_slug} import (
     {method_name},
 )
 
@@ -75,4 +86,10 @@ def test_{method_name}():
 
 
 if __name__ == "__main__":
-    setup_day()
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Set up directory for a problem.")
+    parser.add_argument("--year", default="")
+    args = parser.parse_args()
+
+    setup_day(year=args.year)
