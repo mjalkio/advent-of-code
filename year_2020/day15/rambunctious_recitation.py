2d13135188ab686504bc50030c28150dd276831f
Michael Jalkio <mjalkio@gmail.com>
2021-12-14 02:09:16 -0800

    Attempt to speed up year 2020 day 15 even more

--- year_2020/day15/rambunctious_recitation.py
+++ year_2020/day15/rambunctious_recitation.py
@@ -5,21 +5,20 @@ PUZZLE_INPUT = "15,12,0,14,3,1"
 
 def get_number_spoken(puzzle_input, num_turns=2020):
     starting_numbers = [int(num) for num in puzzle_input.split(",")]
-    num_starting_numbers = len(starting_numbers)
-    last_number_spoken = None
-    times_spoken = {}
-    for turn in range(num_turns):
+    if num_turns <= len(starting_numbers):
+        return starting_numbers[num_turns - 1]
+
+    last_number_spoken = starting_numbers[-1]
+    times_spoken = {starting_numbers[i]: (i + 1, i + 1) for i in range(len(starting_numbers) - 1)}
+    for turn in range(len(starting_numbers), num_turns):
         if last_number_spoken not in times_spoken:
             times_spoken[last_number_spoken] = (turn, turn)
         else:
             last_last_time_spoken = times_spoken[last_number_spoken][0]
             times_spoken[last_number_spoken] = (turn, last_last_time_spoken)
 
-        if turn < num_starting_numbers:
-            last_number_spoken = starting_numbers[turn]
-        else:
-            last_time_spoken, last_last_time_spoken = times_spoken[last_number_spoken]
-            last_number_spoken = last_time_spoken - last_last_time_spoken
+        last_time_spoken, last_last_time_spoken = times_spoken[last_number_spoken]
+        last_number_spoken = last_time_spoken - last_last_time_spoken
 
     return last_number_spoken
 
