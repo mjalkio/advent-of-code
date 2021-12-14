b786c8e8065fdaa7ef56e0a419866ad4dd43a772
Michael Jalkio <mjalkio@gmail.com>
2021-12-13 21:27:04 -0800

    Add day 14 part 2 outline

--- year_2021/day14/extended_polymerization.py
+++ year_2021/day14/extended_polymerization.py
@@ -24,4 +24,6 @@ if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_difference_most_least_common_element_counts(puzzle_input)}")
-    print(f"Part 2: {get_difference_most_least_common_element_counts(puzzle_input)}")
+    print(
+        f"Part 2: {get_difference_most_least_common_element_counts(puzzle_input, num_steps=40)}"
+    )
--- year_2021/day14/test_extended_polymerization.py
+++ year_2021/day14/test_extended_polymerization.py
@@ -1,13 +1,23 @@
+import pytest
+
 from util import read_puzzle_input
 from year_2021.day14.extended_polymerization import (
     get_difference_most_least_common_element_counts,
 )
 
 
-def test_get_difference_most_least_common_element_counts():
+@pytest.mark.parametrize(
+    "num_steps,expected_output",
+    [
+        (10, 1588),
+        (40, 2188189693529),
+    ],
+)
+def test_get_difference_most_least_common_element_counts(num_steps, expected_output):
     assert (
         get_difference_most_least_common_element_counts(
-            read_puzzle_input("test_input.txt")
+            puzzle_input=read_puzzle_input("test_input.txt"),
+            num_steps=num_steps,
         )
-        == 1588
+        == expected_output
     )
