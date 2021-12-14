0a37a72fdb0453342d76672071ed0f14c2314a83
Michael Jalkio <mjalkio@gmail.com>
2021-12-14 02:20:42 -0800

    Mark slow tests again

--- year_2020/day17/test_day17.py
+++ year_2020/day17/test_day17.py
@@ -1,3 +1,5 @@
+import pytest
+
 from year_2020.day17.conway_cubes import get_num_active_cubes, get_num_active_cubes_4d
 
 TEST_INPUT = """
@@ -11,5 +13,6 @@ def test_get_num_active_cubes():
     assert get_num_active_cubes(TEST_INPUT) == 112
 
 
+@pytest.mark.slow
 def test_get_num_active_cubes_4d():
     assert get_num_active_cubes_4d(TEST_INPUT) == 848
--- year_2020/day23/test_day23.py
+++ year_2020/day23/test_day23.py
@@ -10,6 +10,7 @@ def test_get_crap_cups(num_moves, expected):
     assert get_crab_cups(TEST_INPUT, num_moves=num_moves) == expected
 
 
+@pytest.mark.slow
 def test_get_crap_cups_part_two():
     assert (
         get_crab_cups(TEST_INPUT, num_moves=10_000_000, is_part_two=True)
