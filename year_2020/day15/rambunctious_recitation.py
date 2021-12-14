a4ce527bbdd275ef8a99d4dca3285f8b17c276b6
Michael Jalkio <mjalkio@gmail.com>
2021-12-14 01:59:17 -0800

    Attempt to speed up year 2020 day 15

--- year_2020/day15/rambunctious_recitation.py
+++ year_2020/day15/rambunctious_recitation.py
@@ -5,24 +5,25 @@ PUZZLE_INPUT = "15,12,0,14,3,1"
 
 def get_number_spoken(puzzle_input, num_turns=2020):
     starting_numbers = [int(num) for num in puzzle_input.split(",")]
+    num_starting_numbers = len(starting_numbers)
     last_number_spoken = None
-    times_spoken = defaultdict(list)
+    times_spoken = {}
     for turn in range(num_turns):
-        times_spoken[last_number_spoken].append(turn)
-
-        if turn < len(starting_numbers):
-            last_number_spoken = starting_numbers[turn]
-        elif len(times_spoken[last_number_spoken]) == 1:
-            last_number_spoken = 0
+        if last_number_spoken not in times_spoken:
+            times_spoken[last_number_spoken] = (turn, turn)
         else:
-            last_number_times_spoken = times_spoken[last_number_spoken]
-            last_number_spoken = (
-                last_number_times_spoken[-1] - last_number_times_spoken[-2]
-            )
+            last_last_time_spoken = times_spoken[last_number_spoken][0]
+            times_spoken[last_number_spoken] = (turn, last_last_time_spoken)
+
+        if turn < num_starting_numbers:
+            last_number_spoken = starting_numbers[turn]
+        else:
+            last_time_spoken, last_last_time_spoken = times_spoken[last_number_spoken]
+            last_number_spoken = last_time_spoken - last_last_time_spoken
 
     return last_number_spoken
 
 
 if __name__ == "__main__":
     print(f"Part 1: {get_number_spoken(PUZZLE_INPUT)}")
-    print(f"Part 2: {get_number_spoken(PUZZLE_INPUT, num_turns=30000000)}")
+    print(f"Part 2: {get_number_spoken(PUZZLE_INPUT, num_turns=30_000_000)}")
