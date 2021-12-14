c8961c33c04167cb030cee76ddcf0b177c835956
Michael Jalkio <mjalkio@gmail.com>
2021-12-13 23:49:26 -0800

    Solve year 2019 day 1 part 2

--- year_2019/day01/test_the_tyranny_of_the_rocket_equation.py
+++ year_2019/day01/test_the_tyranny_of_the_rocket_equation.py
@@ -1,6 +1,7 @@
 from util import read_puzzle_input
 from year_2019.day01.the_tyranny_of_the_rocket_equation import (
     get_total_fuel_required,
+    get_total_fuel_required_double_check,
 )
 
 
@@ -8,4 +9,11 @@ def test_get_total_fuel_required():
     assert (
         get_total_fuel_required(read_puzzle_input("test_input.txt"))
         == 2 + 2 + 654 + 33583
+    )
+
+
+def test_get_total_fuel_required_double_check():
+    assert (
+        get_total_fuel_required_double_check(read_puzzle_input("test_input.txt"))
+        == 2 + 2 + 966 + 50346
     )
--- year_2019/day01/the_tyranny_of_the_rocket_equation.py
+++ year_2019/day01/the_tyranny_of_the_rocket_equation.py
@@ -6,8 +6,19 @@ def get_total_fuel_required(puzzle_input):
     return sum(mass // 3 - 2 for mass in masses)
 
 
+def get_total_fuel_required_double_check(puzzle_input):
+    masses = [int(mass) for mass in puzzle_input.split('\n')]
+    initial_fuel_requirements = [mass // 3 - 2 for mass in masses]
+    total_fuel = 0
+    for fuel in initial_fuel_requirements:
+        while fuel > 0:
+            total_fuel += fuel
+            fuel = fuel // 3 - 2
+    return total_fuel
+
+
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_total_fuel_required(puzzle_input)}")
-    print(f"Part 2: {get_total_fuel_required(puzzle_input)}")
+    print(f"Part 2: {get_total_fuel_required_double_check(puzzle_input)}")
