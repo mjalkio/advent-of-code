0ac6342f986315069364788533caf292182a7b28
Michael Jalkio <mjalkio@gmail.com>
2021-12-15 10:25:00 -0800

    Add incorrect part 2

--- year_2021/day15/chiton.py
+++ year_2021/day15/chiton.py
@@ -4,8 +4,20 @@ from util import read_puzzle_input
 
 Path = namedtuple("Path", ["positions", "risk"])
 
+MAX_POSITION_RISK = 9
 # Arbitrary large number
-MAX_RISK = 1_000_000_000
+MAX_TOTAL_RISK = 1_000_000_000
+NUM_TIMES_LARGER = 5
+
+
+def _print_map(risk_map):
+    width = max(x for x, _ in risk_map) + 1
+    height = max(y for _, y in risk_map) + 1
+    for y in range(height):
+        line = []
+        for x in range(width):
+            line.append(str(risk_map[(x, y)]))
+        print("".join(line))
 
 
 def get_lowest_risk_path_risk(puzzle_input, use_full_map=False):
@@ -15,17 +27,35 @@ def get_lowest_risk_path_risk(puzzle_input, use_full_map=False):
         for x in range(len(lines[y])):
             risk_map[(x, y)] = int(lines[y][x])
 
+    if use_full_map:
+        width = x + 1
+        height = y + 1
+        for x in range(width):
+            for y in range(height):
+                for dx in range(0, NUM_TIMES_LARGER):
+                    for dy in range(0, NUM_TIMES_LARGER):
+                        if (dx, dy) == (0, 0):
+                            continue
+
+                        position = (x + dx * width, y + dy * height)
+                        added_risk = dx + 1 if dx == dy else max(dx, dy)
+                        risk = (risk_map[x, y] + added_risk) % (MAX_POSITION_RISK + 1)
+                        risk_map[position] = risk
+
+    _print_map(risk_map)
+    width = max(x for x, y in risk_map.keys()) + 1
+    height = max(y for x, y in risk_map.keys()) + 1
     risk_to_destination = {}
-    for x in reversed(range(len(lines[0]))):
-        for y in reversed(range(len(lines))):
+    for x in reversed(range(width)):
+        for y in reversed(range(height)):
             if len(risk_to_destination) == 0:
                 risk_to_destination[(x, y)] = risk_map[(x, y)]
                 continue
 
             risk_to_destination[(x, y)] = (
                 min(
-                    risk_to_destination.get((x + 1, y), MAX_RISK),
-                    risk_to_destination.get((x, y + 1), MAX_RISK),
+                    risk_to_destination.get((x + 1, y), MAX_TOTAL_RISK),
+                    risk_to_destination.get((x, y + 1), MAX_TOTAL_RISK),
                 )
                 + risk_map[(x, y)]
             )
