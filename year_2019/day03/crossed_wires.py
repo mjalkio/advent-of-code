339142e8974fc2071413f563a598e55b0a82acfd
Michael Jalkio <mjalkio@gmail.com>
2021-12-17 23:02:12 -0500

    Solve 2019 day 3 part 2

--- year_2019/day03/crossed_wires.py
+++ year_2019/day03/crossed_wires.py
@@ -3,7 +3,9 @@ from util import read_puzzle_input
 
 def _get_wire_points(wire):
     points = set()
+    num_steps_to_point = {}
     location = [0, 0]
+    num_steps = 0
     for segment in wire.split(","):
         orientation = segment[0]
         length = int(segment[1:])
@@ -21,8 +23,13 @@ def _get_wire_points(wire):
             direction = -1
         for _ in range(length):
             location[axis] += direction
-            points.add(tuple(location))
-    return points
+            num_steps += 1
+            point = tuple(location)
+            points.add(point)
+            if point not in num_steps_to_point:
+                num_steps_to_point[point] = num_steps
+
+    return points, num_steps_to_point
 
 
 def _manhattan_distance(point_a, point_b=(0, 0)):
@@ -31,14 +38,25 @@ def _manhattan_distance(point_a, point_b=(0, 0)):
 
 def get_distance_closest_intersection(puzzle_input):
     wire_1, wire_2 = puzzle_input.split("\n")
-    wire_1_points = _get_wire_points(wire_1)
-    wire_2_points = _get_wire_points(wire_2)
+    wire_1_points, _ = _get_wire_points(wire_1)
+    wire_2_points, _ = _get_wire_points(wire_2)
     intersections = wire_1_points.intersection(wire_2_points)
     return min(_manhattan_distance(point) for point in intersections)
+
+
+def get_fewest_combined_steps(puzzle_input):
+    wire_1, wire_2 = puzzle_input.split("\n")
+    wire_1_points, wire_1_steps_to_point = _get_wire_points(wire_1)
+    wire_2_points, wire_2_steps_to_point = _get_wire_points(wire_2)
+    intersections = wire_1_points.intersection(wire_2_points)
+    return min(
+        wire_1_steps_to_point[point] + wire_2_steps_to_point[point]
+        for point in intersections
+    )
 
 
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_distance_closest_intersection(puzzle_input)}")
-    print(f"Part 2: {get_distance_closest_intersection(puzzle_input)}")
+    print(f"Part 2: {get_fewest_combined_steps(puzzle_input)}")
--- year_2019/day03/test_crossed_wires.py
+++ year_2019/day03/test_crossed_wires.py
@@ -3,6 +3,7 @@ import pytest
 from util import read_puzzle_input
 from year_2019.day03.crossed_wires import (
     get_distance_closest_intersection,
+    get_fewest_combined_steps,
 )
 
 
@@ -15,3 +16,11 @@ def test_get_distance_closest_intersection(input_file, expected_output):
         get_distance_closest_intersection(read_puzzle_input(input_file))
         == expected_output
     )
+
+
+@pytest.mark.parametrize(
+    "input_file,expected_output",
+    [("test_input.txt", 30), ("test_input2.txt", 610), ("test_input3.txt", 410)],
+)
+def test_get_fewest_combined_steps(input_file, expected_output):
+    assert get_fewest_combined_steps(read_puzzle_input(input_file)) == expected_output
