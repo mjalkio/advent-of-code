f433ca06ce55be596fbe6097e13c3a415ba0bfbc
Michael Jalkio <mjalkio@gmail.com>
2021-12-18 12:05:38 -0500

    Add tons of tests

--- year_2021/day18/snailfish.py
+++ year_2021/day18/snailfish.py
@@ -1,6 +1,22 @@
 from util import read_puzzle_input
 
 
+def snailfish_magnitude(number):
+    return 0
+
+
+def snailfish_sum(puzzle_input):
+    return "[0,0]"
+
+
+def snailfish_add(number_a, number_b, reduce_result=True):
+    return "[0,0]"
+
+
+def snailfish_reduce(number):
+    return "[0,0]"
+
+
 def do_homework(puzzle_input):
     return 0
 
