d6781f54f7e9d2084ea9cae7b94a26926ff01428
Michael Jalkio <mjalkio@gmail.com>
2021-12-17 20:20:25 -0500

    Solve day 17 part 2

--- year_2021/day17/trick_shot.py
+++ year_2021/day17/trick_shot.py
@@ -1,7 +1,9 @@
 from util import read_puzzle_input
 
 
-MAX_Y_VEL = 200  # Random large value to restrict the search space
+# In part 1 this was a random large number to restrict the search space
+# Now it's the actual max I saw in part 1
+MAX_Y_VEL = 156
 
 
 def get_highest_possible_y_position(puzzle_input):
@@ -32,7 +34,28 @@ def get_highest_possible_y_position(puzzle_input):
 
 
 def get_num_valid_initial_velocities(puzzle_input):
-    return 0
+    puzzle_input_left, y_str = puzzle_input.split(", y=")
+    _, x_str = puzzle_input_left.split(" x=")
+    x_range = [int(pos) for pos in x_str.split("..")]
+    y_range = [int(pos) for pos in y_str.split("..")]
+
+    num_valid = 0
+    for test_y_vel in range(y_range[0], MAX_Y_VEL):
+        for x_vel in range(x_range[1] + 1):
+            x = 0
+            y = 0
+            y_vel = test_y_vel
+            while x <= x_range[1] and y_range[0] <= y:
+                if x_range[0] <= x <= x_range[1] and y_range[0] <= y <= y_range[1]:
+                    num_valid += 1
+                    break
+                x += x_vel
+                y += y_vel
+                if x_vel > 0:
+                    x_vel -= 1
+                y_vel -= 1
+
+    return num_valid
 
 
 if __name__ == "__main__":
