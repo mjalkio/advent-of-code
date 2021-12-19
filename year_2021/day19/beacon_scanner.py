82a8f59ac9dfba5d6c567db5e7fd229975aa7027
Michael Jalkio <mjalkio@gmail.com>
2021-12-19 13:20:34 -0500

    Add code to generate all orientations of beacons

--- year_2021/day19/beacon_scanner.py
+++ year_2021/day19/beacon_scanner.py
@@ -1,7 +1,58 @@
+from collections import namedtuple
+
 from util import read_puzzle_input
+
+NUM_ORIENTATIONS = 24
+Point = namedtuple("Point", ["x", "y", "z"])
+
+
+def _get_all_orientations(beacons):
+    all_orientations = [[]] * NUM_ORIENTATIONS
+    for x, y, z in beacons:
+        all_point_orientations = [
+            Point(x=x, y=y, z=z),
+            Point(x=x, y=-z, z=y),
+            Point(x=x, y=-y, z=-z),
+            Point(x=x, y=z, z=-y),
+            Point(x=y, y=-x, z=z),
+            Point(x=y, y=-z, z=-x),
+            Point(x=y, y=x, z=-z),
+            Point(x=y, y=z, z=x),
+            Point(x=-x, y=-y, z=z),
+            Point(x=-x, y=-z, z=-y),
+            Point(x=-x, y=y, z=-z),
+            Point(x=-x, y=z, z=y),
+            Point(x=-y, y=x, z=z),
+            Point(x=-y, y=-z, z=x),
+            Point(x=-y, y=-x, z=-z),
+            Point(x=-y, y=z, z=-x),
+            Point(x=z, y=y, z=-x),
+            Point(x=z, y=x, z=y),
+            Point(x=z, y=-y, z=x),
+            Point(x=z, y=-x, z=-y),
+            Point(x=-z, y=y, z=x),
+            Point(x=-z, y=-x, z=y),
+            Point(x=-z, y=-y, z=-x),
+            Point(x=-z, y=x, z=-y),
+        ]
+        for i in range(NUM_ORIENTATIONS):
+            all_orientations[i].append(all_point_orientations[i])
+
+    return all_orientations
 
 
 def get_num_beacons(puzzle_input):
+    lines = puzzle_input.split("\n")
+    scanner_beacons = []
+    for line in lines:
+        if line == "":
+            continue
+        if "scanner" in line:
+            scanner_beacons.append([])
+        else:
+            x, y, z = [int(val) for val in line.split(",")]
+            scanner_beacons[-1].append(Point(x=x, y=y, z=z))
+
     return 0
 
 
