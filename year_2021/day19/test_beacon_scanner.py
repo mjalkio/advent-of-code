58761a049a5044f6b86fac3ef21ae550bdd9bc91
Michael Jalkio <mjalkio@gmail.com>
2021-12-19 10:59:10 -0500

    Add day 19 part 1 outline

---
+++ year_2021/day19/test_beacon_scanner.py
@@ -0,0 +1,8 @@
+from util import read_puzzle_input
+from year_2021.day19.beacon_scanner import (
+    get_num_beacons,
+)
+
+
+def test_get_num_beacons():
+    assert get_num_beacons(read_puzzle_input("test_input.txt")) == 79
