87b4844a72832a9f5462703a05876273ee115304
Michael Jalkio <mjalkio@gmail.com>
2021-12-23 13:13:51 -0500

    Create rough outline of DFS solution

--- year_2021/day23/amphipod.py
+++ year_2021/day23/amphipod.py
@@ -1,8 +1,29 @@
 from util import read_puzzle_input
 
 
+def _get_initial_state(puzzle_input):
+    return None
+
+
+def _get_next_states(state):
+    return []
+
+
+def _is_organized(state):
+    return False
+
+
 def get_minimum_energy_required(puzzle_input):
-    return 0
+    minimum_energy = 1_000_000_000
+    states = [_get_initial_state(puzzle_input)]
+    while len(states) > 0:
+        curr_state = states.pop()
+        for next_state in _get_next_states(curr_state):
+            if _is_organized(curr_state):
+                minimum_energy = min(minimum_energy, curr_state.total_energy)
+            else:
+                states.append(next_state)
+    return minimum_energy
 
 
 if __name__ == "__main__":
