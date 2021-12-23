d0a1b199cfac1bf920dc2ab10093f77e3961d482
Michael Jalkio <mjalkio@gmail.com>
2021-12-23 14:35:54 -0500

    Partially handle case where amphipod can move into room

--- year_2021/day23/amphipod.py
+++ year_2021/day23/amphipod.py
@@ -6,7 +6,7 @@ A = "A"
 B = "B"
 C = "C"
 D = "D"
-HALLWAY_LOCATIONS = {
+ROOM_LOCATIONS = {
     A: 2,
     B: 4,
     C: 6,
@@ -18,28 +18,67 @@ State = namedtuple("State", ["locations", "total_energy"])
 
 def _get_initial_state(puzzle_input):
     lines = puzzle_input.split("\n")
-    hallway_lines = lines[2:-1]
+    room_lines = lines[2:-1]
+    room_depth = len(room_lines)
     locations = {}
-    for i in range(len(hallway_lines)):
-        for j in HALLWAY_LOCATIONS.values():
-            locations[(j, i + 1)] = hallway_lines[i][j + WALL_THICKNESS]
-    return State(locations=locations, total_energy=0)
+    for i in range(len(room_lines)):
+        for j in ROOM_LOCATIONS.values():
+            locations[(j, i + 1)] = room_lines[i][j + WALL_THICKNESS]
+    return (State(locations=locations, total_energy=0), room_depth)
 
 
-def _get_next_states(state):
+def _get_move_cost(start_x, start_y, dest_x, dest_y, locations):
+    return None
+
+
+def _get_next_states(state, room_depth):
+    # If any amphipod can move into its terminal state, make that move
+    for (start_x, start_y), amphipod in state.locations:
+        if start_y != 0:
+            # We do not allow room -> room movement
+            continue
+        dest_x = ROOM_LOCATIONS[amphipod]
+        # Prefer to settle all the way in the back of the room
+        for dest_y in reversed(range(1, room_depth + 1)):
+            move_cost = _get_move_cost(
+                start_x=start_x,
+                start_y=start_y,
+                dest_x=dest_x,
+                dest_y=dest_y,
+                locations=state.locations,
+            )
+            if move_cost is not None:
+                new_locations = state.locations.copy()
+                del new_locations[loc]
+                new_locations[(dest_x, dest_y)] = amphipod
+                return [
+                    State(
+                        locations=new_locations,
+                        total_energy=state.total_energy + move_cost,
+                    )
+                ]
+
+    # Otherwise make all moves that are valid
     return []
 
 
 def _is_organized(state):
-    return False
+    for (x, y), amphipod in state.locations:
+        if y == 0:
+            return False
+        if x != ROOM_LOCATIONS[amphipod]:
+            return False
+
+    return True
 
 
 def get_minimum_energy_required(puzzle_input):
     minimum_energy = 1_000_000_000
-    states = [_get_initial_state(puzzle_input)]
+    initial_state, room_depth = _get_initial_state(puzzle_input)
+    states = [initial_state]
     while len(states) > 0:
         curr_state = states.pop()
-        for next_state in _get_next_states(curr_state):
+        for next_state in _get_next_states(curr_state, room_depth):
             if curr_state.total_energy >= minimum_energy:
                 continue
             elif _is_organized(curr_state):
