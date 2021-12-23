716e07a208250deddd6e361e8b35a8b430d9936d
Michael Jalkio <mjalkio@gmail.com>
2021-12-22 19:33:53 -0500

    Mark day 21 test as slow

--- year_2021/day21/test_dirac_dice.py
+++ year_2021/day21/test_dirac_dice.py
@@ -1,3 +1,5 @@
+import pytest
+
 from util import read_puzzle_input
 from year_2021.day21.dirac_dice import (
     get_losing_score_times_num_rolls,
@@ -11,5 +13,6 @@ def test_get_losing_score_times_num_rolls():
     )
 
 
+@pytest.mark.slow
 def test_get_num_universes():
     assert get_num_universes(read_puzzle_input("test_input.txt")) == 444356092776315
