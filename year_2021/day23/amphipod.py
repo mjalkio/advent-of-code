b0855e70e3a0d1e5fc59c20750d9b261699935f8
Michael Jalkio <mjalkio@gmail.com>
2021-12-23 13:38:34 -0500

    Implement _get_initial_state

--- year_2021/day23/amphipod.py
+++ year_2021/day23/amphipod.py
@@ -1,8 +1,29 @@
+from collections import namedtuple
+
 from util import read_puzzle_input
+
+A = "A"
+B = "B"
+C = "C"
+D = "D"
+HALLWAY_LOCATIONS = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8,
+}
+WALL_THICKNESS = 1
+State = namedtuple("State", ["locations", "total_energy"])
 
 
 def _get_initial_state(puzzle_input):
-    return None
+    lines = puzzle_input.split("\n")
+    hallway_lines = lines[2:-1]
+    locations = {}
+    for i in range(len(hallway_lines)):
+        for j in HALLWAY_LOCATIONS.values():
+            locations[(j, i + 1)] = hallway_lines[i][j + WALL_THICKNESS]
+    return State(locations=locations, total_energy=0)
 
 
 def _get_next_states(state):
@@ -19,8 +40,10 @@ def get_minimum_energy_required(puzzle_input):
     while len(states) > 0:
         curr_state = states.pop()
         for next_state in _get_next_states(curr_state):
-            if _is_organized(curr_state):
-                minimum_energy = min(minimum_energy, curr_state.total_energy)
+            if curr_state.total_energy >= minimum_energy:
+                continue
+            elif _is_organized(curr_state):
+                minimum_energy = curr_state.total_energy
             else:
                 states.append(next_state)
     return minimum_energy
