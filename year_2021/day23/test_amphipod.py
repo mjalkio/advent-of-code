3d84a2e5460e404962f99ae16942583bbe5db5df
Michael Jalkio <mjalkio@gmail.com>
2021-12-23 23:56:03 -0500

    Skip super slow tests

--- year_2021/day23/test_amphipod.py
+++ year_2021/day23/test_amphipod.py
@@ -8,7 +8,11 @@ from year_2021.day23.amphipod import (
 
 @pytest.mark.parametrize(
     "input_file,expected_output",
-    [("test_input.txt", 12521), ("test_input_2.txt", 44169)],
+    [
+        pytest.param("test_input.txt", 12521, marks=pytest.mark.slow),
+        # This test is extraordinarily slow, so I want to actually skip it
+        pytest.param("test_input_2.txt", 44169, marks=pytest.mark.skip),
+    ],
 )
 def test_get_minimum_energy_required(input_file, expected_output):
     assert get_minimum_energy_required(read_puzzle_input(input_file)) == expected_output
