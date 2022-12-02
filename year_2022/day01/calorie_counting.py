7e0088b7d9a1bc0e03cc5324eca2b04631dfd389
Michael Jalkio <mjalkio@gmail.com>
2022-12-01 22:00:16 -0800

    Add day 1 part 1 solution

--- year_2022/day01/calorie_counting.py
+++ year_2022/day01/calorie_counting.py
@@ -2,7 +2,12 @@ from util import read_puzzle_input
 
 
 def get_max_total_calories(puzzle_input):
-    return 0
+    elf_snack_calorie_lines = puzzle_input.split("\n\n")
+    elf_snack_calories = []
+    for line in elf_snack_calorie_lines:
+        snack_calories = [int(cal) for cal in line.split("\n")]
+        elf_snack_calories.append(snack_calories)
+    return max([sum(cals) for cals in elf_snack_calories])
 
 
 if __name__ == "__main__":
