42b3541e4a4c35cad6b12ccbae4aff6d5214527f
Michael Jalkio <mjalkio@gmail.com>
2022-12-01 22:21:13 -0800

    Add incorrect solution

--- year_2022/day02/rock_paper_scissors.py
+++ year_2022/day02/rock_paper_scissors.py
@@ -1,8 +1,38 @@
 from util import read_puzzle_input
 
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+OPP_CHOICE_MAP = {
+    "A": ROCK,
+    "B": PAPER,
+    "C": SCISSORS,
+}
+MY_CHOICE_MAP = {
+    "X": ROCK,
+    "Y": PAPER,
+    "Z": SCISSORS,
+}
+
 
 def get_score(puzzle_input):
-    return 0
+    rounds = puzzle_input.split("\n")
+
+    score = 0
+    for rnd in rounds:
+        if rnd == "":
+            continue
+
+        opp, me = rnd.split()
+        opp_choice = OPP_CHOICE_MAP[opp]
+        my_choice = MY_CHOICE_MAP[me]
+        score += my_choice
+        if (my_choice == ROCK and opp_choice == SCISSORS) or my_choice > opp_choice:
+            score += 6
+        elif my_choice == opp_choice:
+            score += 3
+
+    return score
 
 
 if __name__ == "__main__":
