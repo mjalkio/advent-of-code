afec11bb257098bc218ea99ea938cc6f2486079a
Michael Jalkio <mjalkio@gmail.com>
2022-12-01 22:02:16 -0800

    Add day 1 part 2 outline

--- year_2022/day01/calorie_counting.py
+++ year_2022/day01/calorie_counting.py
@@ -10,8 +10,12 @@ def get_max_total_calories(puzzle_input):
     return max([sum(cals) for cals in elf_snack_calories])
 
 
+def get_top_three_total_calories(puzzle_input):
+    return 0
+
+
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_max_total_calories(puzzle_input)}")
-    print(f"Part 2: {get_max_total_calories(puzzle_input)}")
+    print(f"Part 2: {get_top_three_total_calories(puzzle_input)}")
--- year_2022/day01/test_calorie_counting.py
+++ year_2022/day01/test_calorie_counting.py
@@ -1,8 +1,13 @@
 from util import read_puzzle_input
 from year_2022.day01.calorie_counting import (
     get_max_total_calories,
+    get_top_three_total_calories,
 )
 
 
 def test_get_max_total_calories():
     assert get_max_total_calories(read_puzzle_input("test_input.txt")) == 24000
+
+
+def test_get_top_three_total_calories():
+    assert get_top_three_total_calories(read_puzzle_input("test_input.txt")) == 45000
