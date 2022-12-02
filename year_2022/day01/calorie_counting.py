07b58c89271090ce1341651599ab5a5b915af036
Michael Jalkio <mjalkio@gmail.com>
2022-12-01 22:08:48 -0800

    Solve day 1 part 2

--- year_2022/day01/calorie_counting.py
+++ year_2022/day01/calorie_counting.py
@@ -1,17 +1,24 @@
 from util import read_puzzle_input
 
 
-def get_max_total_calories(puzzle_input):
+def _get_elf_snack_calories(puzzle_input):
     elf_snack_calorie_lines = puzzle_input.split("\n\n")
     elf_snack_calories = []
     for line in elf_snack_calorie_lines:
         snack_calories = [int(cal) for cal in line.split("\n")]
         elf_snack_calories.append(snack_calories)
+    return elf_snack_calories
+
+
+def get_max_total_calories(puzzle_input):
+    elf_snack_calories = _get_elf_snack_calories(puzzle_input)
     return max([sum(cals) for cals in elf_snack_calories])
 
 
 def get_top_three_total_calories(puzzle_input):
-    return 0
+    elf_snack_calories = _get_elf_snack_calories(puzzle_input)
+    sorted_total_calories = sorted([sum(cals) for cals in elf_snack_calories])
+    return sum(list(sorted_total_calories)[-3:])
 
 
 if __name__ == "__main__":
