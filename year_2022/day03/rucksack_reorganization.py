4dcbfc495ae03427c2e0f9dbe0fe9bf492ad49da
Michael Jalkio <mjalkio@gmail.com>
2022-12-02 22:18:48 -0800

    Add day 3 part 2 outline

--- year_2022/day03/rucksack_reorganization.py
+++ year_2022/day03/rucksack_reorganization.py
@@ -23,8 +23,12 @@ def sum_of_common_priorities(puzzle_input):
     return answer
 
 
+def sum_of_badge_priorities(puzzle_input):
+    return 0
+
+
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {sum_of_common_priorities(puzzle_input)}")
-    print(f"Part 2: {sum_of_common_priorities(puzzle_input)}")
+    print(f"Part 2: {sum_of_badge_priorities(puzzle_input)}")
--- year_2022/day03/test_rucksack_reorganization.py
+++ year_2022/day03/test_rucksack_reorganization.py
@@ -1,8 +1,13 @@
 from util import read_puzzle_input
 from year_2022.day03.rucksack_reorganization import (
+    sum_of_badge_priorities,
     sum_of_common_priorities,
 )
 
 
 def test_sum_of_common_priorities():
     assert sum_of_common_priorities(read_puzzle_input("test_input.txt")) == 157
+
+
+def test_sum_of_badge_priorities():
+    assert sum_of_badge_priorities(read_puzzle_input("test_input.txt")) == 70
