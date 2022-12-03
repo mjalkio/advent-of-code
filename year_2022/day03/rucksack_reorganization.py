f33faaf9102d5882586fa43b7b9edba33d0bdce1
Michael Jalkio <mjalkio@gmail.com>
2022-12-02 22:16:07 -0800

    Add day 3 part 1 answer

--- year_2022/day03/rucksack_reorganization.py
+++ year_2022/day03/rucksack_reorganization.py
@@ -2,7 +2,25 @@ from util import read_puzzle_input
 
 
 def sum_of_common_priorities(puzzle_input):
-    return 0
+    answer = 0
+    for line in puzzle_input.split("\n"):
+        if line == "":
+            continue
+
+        assert len(line) % 2 == 0
+        first = line[: len(line) // 2]
+        second = line[len(line) // 2 :]
+        common = set(first).intersection(set(second))
+        assert len(common) == 1
+        common_item = common.pop()
+
+        if common_item >= "a":
+            # Lowercase letter
+            answer += ord(common_item) - ord("a") + 1
+        else:
+            # Uppercase letter
+            answer += ord(common_item) - ord("A") + 1 + 26
+    return answer
 
 
 if __name__ == "__main__":
