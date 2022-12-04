95ce2c43d33c9f5adb87f9eb29b63450078e710e
Michael Jalkio <mjalkio@gmail.com>
2022-12-03 21:58:31 -0800

    Solve day 4 part 1

--- year_2022/day04/camp_cleanup.py
+++ year_2022/day04/camp_cleanup.py
@@ -2,7 +2,20 @@ from util import read_puzzle_input
 
 
 def num_fully_contained_assignments(puzzle_input):
-    return 0
+    num = 0
+    for pair in puzzle_input.split("\n"):
+        if pair == "":
+            continue
+
+        assignment_1, assignment_2 = pair.split(",")
+        ass_1_start, ass_1_end = [int(num) for num in assignment_1.split("-")]
+        ass_2_start, ass_2_end = [int(num) for num in assignment_2.split("-")]
+
+        if ass_1_start <= ass_2_start and ass_1_end >= ass_2_end:
+            num += 1
+        elif ass_2_start <= ass_1_start and ass_2_end >= ass_1_end:
+            num += 1
+    return num
 
 
 if __name__ == "__main__":
