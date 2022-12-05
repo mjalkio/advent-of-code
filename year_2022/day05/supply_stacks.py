5804ab7eae58a3fed2e82a9f414baf3ca1e19482
Michael Jalkio <mjalkio@gmail.com>
2022-12-05 14:14:42 +0000

    Parse starting stacks

--- year_2022/day05/supply_stacks.py
+++ year_2022/day05/supply_stacks.py
@@ -3,8 +3,23 @@ from util import read_puzzle_input
 
 def get_top_crates(puzzle_input):
     starting_stacks, procedure = puzzle_input.split("\n\n")
+
     starting_stack_lines = starting_stacks.split("\n")
     num_stacks = len(starting_stack_lines.pop().split("   "))
+    stacks = []
+    for _ in range(num_stacks):
+        stacks.append([])
+
+    for line in starting_stack_lines:
+        i = 0
+        while len(line) > 0:
+            crate, line = line[1], line[4:]
+            if crate != " ":
+                stacks[i].append(crate)
+            i += 1
+    for stack in stacks:
+        stack.reverse()
+
     return 0
 
 
