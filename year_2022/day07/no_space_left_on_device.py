b0d62dbcad5c69afdf039ebaf756728fc262b266
Michael Jalkio <mjalkio@gmail.com>
2022-12-08 17:42:25 -0800

    Pass day 7 part 1 test

--- year_2022/day07/no_space_left_on_device.py
+++ year_2022/day07/no_space_left_on_device.py
@@ -3,6 +3,9 @@ from collections import defaultdict
 from util import read_puzzle_input
 
 
+SMALL_DIRECTORY_SIZE = 100_000
+
+
 def sum_total_size_small_directories(puzzle_input):
     lines = [line for line in puzzle_input.split("\n") if line != ""]
     assert lines[0] == "$ cd /"
@@ -10,12 +13,13 @@ def sum_total_size_small_directories(puzzle_input):
     current_dir = "/"
     all_dirs = set("/")
     parent_dirs = {}
-    direct_files_size = defaultdict(int)
+    total_sizes = defaultdict(int)
     subdirs = defaultdict(list)
 
     i = 1
     while i < len(lines):
         if lines[i] == "$ ls":
+            # Note: I am assuming we don't `ls` the same directory twice
             i += 1
             while i < len(lines) and not lines[i].startswith("$"):
                 if lines[i].startswith("dir "):
@@ -25,7 +29,13 @@ def sum_total_size_small_directories(puzzle_input):
                     parent_dirs[subdir] = current_dir
                 else:
                     file_size, file_name = lines[i].split()
-                    direct_files_size[current_dir] += int(file_size)
+                    total_sizes[current_dir] += int(file_size)
+
+                    parent_dir = parent_dirs.get(current_dir)
+                    while parent_dir is not None:
+                        total_sizes[parent_dir] += int(file_size)
+                        parent_dir = parent_dirs.get(parent_dir)
+
                 i += 1
         elif lines[i] == "$ cd ..":
             current_dir = parent_dirs[current_dir]
@@ -34,7 +44,7 @@ def sum_total_size_small_directories(puzzle_input):
             assert lines[i].startswith("$ cd ")
             current_dir = lines[i][5:]
             i += 1
-    return 0
+    return sum(size for size in total_sizes.values() if size <= SMALL_DIRECTORY_SIZE)
 
 
 if __name__ == "__main__":
