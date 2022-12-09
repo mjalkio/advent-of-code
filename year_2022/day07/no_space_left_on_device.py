7b7df63031f984588759c79bc5980b93c2c145ce
Michael Jalkio <mjalkio@gmail.com>
2022-12-08 18:06:50 -0800

    Solve day 7 part 2

--- year_2022/day07/no_space_left_on_device.py
+++ year_2022/day07/no_space_left_on_device.py
@@ -9,7 +9,7 @@ TOTAL_DISK_SIZE = 70_000_000
 UPDATE_SIZE = 30_000_000
 
 
-def sum_total_size_small_directories(puzzle_input):
+def _get_directory_sizes(puzzle_input):
     lines = [line for line in puzzle_input.split("\n") if line != ""]
     assert lines[0] == "$ cd /"
 
@@ -38,11 +38,21 @@ def sum_total_size_small_directories(puzzle_input):
             assert lines[i].startswith("$ cd ")
             current_dir = Path(current_dir, lines[i][5:])
             i += 1
+    return dir_sizes
+
+
+def sum_total_size_small_directories(puzzle_input):
+    dir_sizes = _get_directory_sizes(puzzle_input)
     return sum(size for size in dir_sizes.values() if size <= SMALL_DIRECTORY_SIZE)
 
 
 def get_directory_to_delete_size(puzzle_input):
-    return 0
+    dir_sizes = _get_directory_sizes(puzzle_input)
+    root_dir = Path("/")
+    used_space = dir_sizes[root_dir]
+    space_available = TOTAL_DISK_SIZE - used_space
+    additional_space_needed = UPDATE_SIZE - space_available
+    return min(size for size in dir_sizes.values() if size >= additional_space_needed)
 
 
 if __name__ == "__main__":
