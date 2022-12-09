a583cc3c7b61e8d4257124f1d1910871f25dba81
Michael Jalkio <mjalkio@gmail.com>
2022-12-08 18:23:00 -0800

    Parse tree heights

--- year_2022/day08/treetop_tree_house.py
+++ year_2022/day08/treetop_tree_house.py
@@ -2,6 +2,14 @@ from util import read_puzzle_input
 
 
 def num_visible_trees(puzzle_input):
+    lines = [line for line in puzzle_input.split("\n") if line != ""]
+    num_rows = len(lines)
+    num_cols = len(lines[0])
+
+    tree_height_map = {}
+    for i in range(num_rows):
+        for j in range(num_cols):
+            tree_height_map[(i, j)] = int(lines[i][j])
     return 0
 
 
