54e009687494ab634dd918cc60cac347d8ee0bb5
Michael Jalkio <mjalkio@gmail.com>
2022-12-08 17:55:24 -0800

    Solve day 7 part 1

--- year_2022/day07/no_space_left_on_device.py
+++ year_2022/day07/no_space_left_on_device.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from pathlib import Path
 
 from util import read_puzzle_input
 
@@ -10,11 +11,8 @@ def sum_total_size_small_directories(puzzle_input):
     lines = [line for line in puzzle_input.split("\n") if line != ""]
     assert lines[0] == "$ cd /"
 
-    current_dir = "/"
-    all_dirs = set("/")
-    parent_dirs = {}
+    current_dir = Path("/")
     total_sizes = defaultdict(int)
-    subdirs = defaultdict(list)
 
     i = 1
     while i < len(lines):
@@ -23,26 +21,20 @@ def sum_total_size_small_directories(puzzle_input):
             i += 1
             while i < len(lines) and not lines[i].startswith("$"):
                 if lines[i].startswith("dir "):
-                    subdir = lines[i][4:]
-                    all_dirs.add(subdir)
-                    subdirs[current_dir].append(subdir)
-                    parent_dirs[subdir] = current_dir
+                    pass
                 else:
                     file_size, file_name = lines[i].split()
                     total_sizes[current_dir] += int(file_size)
 
-                    parent_dir = parent_dirs.get(current_dir)
-                    while parent_dir is not None:
+                    for parent_dir in current_dir.parents:
                         total_sizes[parent_dir] += int(file_size)
-                        parent_dir = parent_dirs.get(parent_dir)
-
                 i += 1
         elif lines[i] == "$ cd ..":
-            current_dir = parent_dirs[current_dir]
+            current_dir = current_dir.parent
             i += 1
         else:
             assert lines[i].startswith("$ cd ")
-            current_dir = lines[i][5:]
+            current_dir = Path(current_dir, lines[i][5:])
             i += 1
     return sum(size for size in total_sizes.values() if size <= SMALL_DIRECTORY_SIZE)
 
