18b8ac975f89cfd28cc8a011fdf5e4d083fdb9d4
Michael Jalkio <mjalkio@gmail.com>
2022-12-09 21:36:22 -0800

    Parse steps

--- year_2022/day09/rope_bridge.py
+++ year_2022/day09/rope_bridge.py
@@ -2,7 +2,14 @@ from util import read_puzzle_input
 
 
 def num_positions_tail_visits(puzzle_input):
-    return 0
+    tail_positions = set()
+    steps = []
+    for line in puzzle_input.split("\n"):
+        if line == "":
+            continue
+        direction, num_steps = line.split()
+        steps += [direction] * int(num_steps)
+    return len(tail_positions)
 
 
 if __name__ == "__main__":
