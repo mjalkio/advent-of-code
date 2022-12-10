0e07a563b0ff88525b573d9181c4811de77e7a9a
Michael Jalkio <mjalkio@gmail.com>
2022-12-09 21:59:35 -0800

    Add incorrect day 9 part 1 solution

--- year_2022/day09/rope_bridge.py
+++ year_2022/day09/rope_bridge.py
@@ -1,14 +1,48 @@
 from util import read_puzzle_input
 
+DIRECTION_MAP = {
+    "R": (1, 0),
+    "L": (-1, 0),
+    "U": (0, 1),
+    "D": (0, -1),
+}
+
+
+def _is_touching(head_x, head_y, tail_x, tail_y):
+    return abs(head_x - head_y) <= 1 and abs(tail_x - tail_y) <= 1
+
 
 def num_positions_tail_visits(puzzle_input):
-    tail_positions = set()
     steps = []
     for line in puzzle_input.split("\n"):
         if line == "":
             continue
         direction, num_steps = line.split()
-        steps += [direction] * int(num_steps)
+        steps += [DIRECTION_MAP[direction]] * int(num_steps)
+
+    head_x = 0
+    head_y = 0
+    tail_x = 0
+    tail_y = 0
+    tail_positions = set([(tail_x, tail_y)])
+    for dx, dy in steps:
+        head_x += dx
+        head_y += dy
+
+        if _is_touching(head_x, head_y, tail_x, tail_y):
+            continue
+
+        if head_x > tail_x:
+            tail_x += 1
+        elif head_x < tail_x:
+            tail_x -= 1
+
+        if head_y > tail_y:
+            tail_y += 1
+        elif head_y < tail_y:
+            tail_y -= 1
+
+        tail_positions.add((tail_x, tail_y))
     return len(tail_positions)
 
 
