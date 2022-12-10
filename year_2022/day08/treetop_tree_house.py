e68ad5e39b09cd153204881b1429a1658541f940
Michael Jalkio <mjalkio@gmail.com>
2022-12-09 21:06:40 -0800

    Add incorrect day 8 part 2 solution

--- year_2022/day08/treetop_tree_house.py
+++ year_2022/day08/treetop_tree_house.py
@@ -31,7 +31,33 @@ def num_visible_trees(puzzle_input):
 
 
 def highest_scenic_score(puzzle_input):
-    return 0
+    lines = [line for line in puzzle_input.split("\n") if line != ""]
+    num_rows = len(lines)
+    num_cols = len(lines[0])
+
+    tree_height_map = {}
+    for i in range(num_rows):
+        for j in range(num_cols):
+            tree_height_map[i, j] = int(lines[i][j])
+
+    highest_scenic_score = 0
+    for i, j in tree_height_map:
+        scenic_score = 1
+
+        for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            num_visible_trees = 1
+            ii = i + di
+            jj = j + dj
+            while (ii, jj) in tree_height_map and tree_height_map[
+                ii, jj
+            ] < tree_height_map[i, j]:
+                num_visible_trees += 1
+                ii += di
+                jj += dj
+            scenic_score *= num_visible_trees
+
+        highest_scenic_score = max(scenic_score, highest_scenic_score)
+    return highest_scenic_score
 
 
 if __name__ == "__main__":
