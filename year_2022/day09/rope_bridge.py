8db258fb6126bf4c9e35c2684897af54baa0c90b
Michael Jalkio <mjalkio@gmail.com>
2022-12-09 22:26:38 -0800

    All tests are off by 1

--- year_2022/day09/rope_bridge.py
+++ year_2022/day09/rope_bridge.py
@@ -8,8 +8,8 @@ DIRECTION_MAP = {
 }
 
 
-def _is_touching(head_x, head_y, tail_x, tail_y):
-    return abs(head_x - tail_x) <= 1 and abs(head_y - tail_y) <= 1
+def _is_touching(x1, y1, x2, y2):
+    return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
 
 
 def num_positions_tail_visits(puzzle_input, num_knots=2):
@@ -20,29 +20,33 @@ def num_positions_tail_visits(puzzle_input, num_knots=2):
         direction, num_steps = line.split()
         steps += [DIRECTION_MAP[direction]] * int(num_steps)
 
-    head_x = 0
-    head_y = 0
-    tail_x = 0
-    tail_y = 0
-    tail_positions = set([(tail_x, tail_y)])
+    knot_positions = [(0, 0)] * num_knots
+    tail_positions = set(knot_positions[-1])
     for dx, dy in steps:
-        head_x += dx
-        head_y += dy
+        knot_positions[0] = (knot_positions[0][0] + dx, knot_positions[0][1] + dy)
 
-        if _is_touching(head_x, head_y, tail_x, tail_y):
-            continue
+        for i in range(1, num_knots):
+            knot_1_x = knot_positions[i - 1][0]
+            knot_1_y = knot_positions[i - 1][1]
+            knot_2_x = knot_positions[i][0]
+            knot_2_y = knot_positions[i][1]
 
-        if head_x > tail_x:
-            tail_x += 1
-        elif head_x < tail_x:
-            tail_x -= 1
+            if _is_touching(knot_1_x, knot_1_y, knot_2_x, knot_2_y):
+                continue
 
-        if head_y > tail_y:
-            tail_y += 1
-        elif head_y < tail_y:
-            tail_y -= 1
+            if knot_1_x > knot_2_x:
+                knot_2_x += 1
+            elif knot_1_x < knot_2_x:
+                knot_2_x -= 1
 
-        tail_positions.add((tail_x, tail_y))
+            if knot_1_y > knot_2_y:
+                knot_2_y += 1
+            elif knot_1_y < knot_2_y:
+                knot_2_y -= 1
+
+            knot_positions[i] = (knot_2_x, knot_2_y)
+
+        tail_positions.add(knot_positions[-1])
     return len(tail_positions)
 
 
