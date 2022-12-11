374c5496586d2e77488f50f7ed0bad772d3da309
Michael Jalkio <mjalkio@gmail.com>
2022-12-10 20:47:43 -0800

    Solve day 10 part 1

--- year_2022/day10/cathode_ray_tube.py
+++ year_2022/day10/cathode_ray_tube.py
@@ -2,7 +2,23 @@ from util import read_puzzle_input
 
 
 def sum_interesting_signal_strengths(puzzle_input):
-    return 0
+    instructions = [line for line in puzzle_input.split("\n") if line != ""]
+    cycle_value = 1
+    X = 1
+    signal_strengths = []
+    for instr in instructions:
+        signal_strengths.append(cycle_value * X)
+        if instr == "noop":
+            cycle_value += 1
+        else:
+            V = int(instr[5:])
+            cycle_value += 1
+            signal_strengths.append(cycle_value * X)
+            X += V
+            cycle_value += 1
+    return sum(
+        signal_strengths[i] for i in range(len(signal_strengths)) if (i - 19) % 40 == 0
+    )
 
 
 if __name__ == "__main__":
