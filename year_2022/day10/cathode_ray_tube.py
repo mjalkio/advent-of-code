806884b86f544870b5a2f1c0c232a06928554f93
Michael Jalkio <mjalkio@gmail.com>
2022-12-11 07:28:43 -0800

    Add incorrect day 10 part 2 solution

--- year_2022/day10/cathode_ray_tube.py
+++ year_2022/day10/cathode_ray_tube.py
@@ -1,21 +1,39 @@
 from util import read_puzzle_input
 
 
+def _render_image(image, pos, X):
+    if pos % 40 == 0:
+        image += "\n"
+
+    if pos % 40 in range(X - 1, X + 2):
+        image += "#"
+    else:
+        image += "."
+    return image
+
+
 def sum_interesting_signal_strengths(puzzle_input):
     instructions = [line for line in puzzle_input.split("\n") if line != ""]
     cycle_value = 1
     X = 1
     signal_strengths = []
+    image = ""
     for instr in instructions:
         signal_strengths.append(cycle_value * X)
         if instr == "noop":
+            image = _render_image(image=image, pos=cycle_value - 1, X=X)
             cycle_value += 1
         else:
             V = int(instr[5:])
+            image = _render_image(image=image, pos=cycle_value - 1, X=X)
             cycle_value += 1
+
             signal_strengths.append(cycle_value * X)
             X += V
+
+            image = _render_image(image=image, pos=cycle_value - 1, X=X)
             cycle_value += 1
+    print(image)
     return sum(
         signal_strengths[i] for i in range(len(signal_strengths)) if (i - 19) % 40 == 0
     )
@@ -25,4 +43,3 @@ if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {sum_interesting_signal_strengths(puzzle_input)}")
-    print(f"Part 2: {sum_interesting_signal_strengths(puzzle_input)}")
