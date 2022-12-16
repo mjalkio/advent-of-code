618ed1f623fbf2226934c6a90f94722135325119
Michael Jalkio <mjalkio@gmail.com>
2022-12-15 22:30:45 -0800

    Add day 12 part 1 outline

---
+++ year_2022/day12/test_hill_climbing_algorithm.py
@@ -0,0 +1,8 @@
+from util import read_puzzle_input
+from year_2022.day12.hill_climbing_algorithm import (
+    fewest_steps,
+)
+
+
+def test_fewest_steps():
+    assert fewest_steps(read_puzzle_input("test_input.txt")) == 31
