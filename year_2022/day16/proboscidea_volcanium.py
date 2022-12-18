40456ea030de8a0a741b85b6696e7f149e464a9f
Michael Jalkio <mjalkio@gmail.com>
2022-12-18 15:21:06 -0800

    Calculate distances between all valves

--- year_2022/day16/proboscidea_volcanium.py
+++ year_2022/day16/proboscidea_volcanium.py
@@ -1,3 +1,5 @@
+import math
+
 from util import read_puzzle_input
 
 
@@ -11,6 +13,22 @@ def most_pressure_possible(puzzle_input):
         flow_rate = int(flow_rate_desc[5:-1])
         flow_rates[valve] = flow_rate
         tunnels[valve] = "".join(words[9:]).split(",")
+
+    # Floyd-Warshall algorithm (or so I hope)
+    distances = {}
+    for valve1 in flow_rates.keys():
+        for valve2 in flow_rates.keys():
+            if valve1 == valve2:
+                distances[valve1, valve2] = 0
+            elif valve2 in tunnels[valve1]:
+                distances[valve1, valve2] = 1
+            else:
+                distances[valve1, valve2] = math.inf
+    for k in flow_rates.keys():
+        for i in flow_rates.keys():
+            for j in flow_rates.keys():
+                if distances[i, j] > distances[i, k] + distances[k, j]:
+                    distances[i, j] = distances[i, k] + distances[k, j]
     return 0
 
 
