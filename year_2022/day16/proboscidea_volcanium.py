84f7c91ebab96fd8f6eff7db91eaf501cc5cd7f8
Michael Jalkio <mjalkio@gmail.com>
2022-12-18 15:02:10 -0800

    Parse input

--- year_2022/day16/proboscidea_volcanium.py
+++ year_2022/day16/proboscidea_volcanium.py
@@ -2,6 +2,15 @@ from util import read_puzzle_input
 
 
 def most_pressure_possible(puzzle_input):
+    flow_rates = {}
+    tunnels = {}
+    for line in puzzle_input.split("\n"):
+        words = line.split()
+        valve = words[1]
+        flow_rate_desc = words[4]
+        flow_rate = int(flow_rate_desc[5:-1])
+        flow_rates[valve] = flow_rate
+        tunnels[valve] = "".join(words[9:]).split(",")
     return 0
 
 
