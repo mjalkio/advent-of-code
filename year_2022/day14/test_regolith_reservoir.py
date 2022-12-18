d191cb9bfdfa72f2bd5b5f4c02f546dc8f0c4dac
Michael Jalkio <mjalkio@gmail.com>
2022-12-18 12:29:38 -0800

    Add day 14 part 1 outline

---
+++ year_2022/day14/test_regolith_reservoir.py
@@ -0,0 +1,8 @@
+from util import read_puzzle_input
+from year_2022.day14.regolith_reservoir import (
+    num_resting_units,
+)
+
+
+def test_num_resting_units():
+    assert num_resting_units(read_puzzle_input("test_input.txt")) == 24
