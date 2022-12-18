bb899c0f81df806c8324d6687a496379b7ec7cf7
Michael Jalkio <mjalkio@gmail.com>
2022-12-18 10:41:59 -0800

    Add is_ordered method

--- year_2022/day13/distress_signal.py
+++ year_2022/day13/distress_signal.py
@@ -1,8 +1,19 @@
 from util import read_puzzle_input
 
 
+def is_ordered(left, right):
+    return False
+
+
 def sum_indices_ordered_pairs(puzzle_input):
-    return 0
+    pairs = puzzle_input.split("\n\n")
+
+    result = 0
+    for i, pair in enumerate(pairs):
+        left, right = pair.split("\n")
+        if is_ordered(left, right):
+            result += i + 1
+    return result
 
 
 if __name__ == "__main__":
--- year_2022/day13/test_distress_signal.py
+++ year_2022/day13/test_distress_signal.py
@@ -1,7 +1,27 @@
+import pytest
+
 from util import read_puzzle_input
 from year_2022.day13.distress_signal import (
+    is_ordered,
     sum_indices_ordered_pairs,
 )
+
+
+@pytest.mark.parametrize(
+    "left, right, expected",
+    [
+        ("[1,1,3,1,1]", "[1,1,5,1,1]", True),
+        ("[[1],[2,3,4]]", "[[1],4]", True),
+        ("[9]", "[[8,7,6]]", False),
+        ("[[4,4],4,4]", "[[4,4],4,4,4]", True),
+        ("[7,7,7,7]", "[7,7,7]", False),
+        ("[]", "[3]", True),
+        ("[[[]]]", "[[]]", False),
+        ("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]", False),
+    ],
+)
+def test_is_ordered(left, right, expected):
+    assert is_ordered(left, right) == expected
 
 
 def test_sum_indices_ordered_pairs():
