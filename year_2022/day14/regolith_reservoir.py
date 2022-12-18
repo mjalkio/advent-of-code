b6ab761dea53b61c7387e05e98c8aa80006c2a2a
Michael Jalkio <mjalkio@gmail.com>
2022-12-18 13:09:40 -0800

    Solve day 14 part 2

--- year_2022/day14/regolith_reservoir.py
+++ year_2022/day14/regolith_reservoir.py
@@ -3,6 +3,9 @@ import math
 from util import read_puzzle_input
 
 SAND_SOURCE = (500, 0)
+ROCK = "#"
+SAND = "o"
+SOURCE = "+"
 
 
 def _print_map(sand_map):
@@ -27,7 +30,7 @@ def _print_map(sand_map):
 
 
 def num_resting_units(puzzle_input, fall_to_floor=False):
-    sand_map = {SAND_SOURCE: "+"}
+    sand_map = {SAND_SOURCE: SOURCE}
 
     lines = puzzle_input.split("\n")
     for line in lines:
@@ -41,20 +44,26 @@ def num_resting_units(puzzle_input, fall_to_floor=False):
                 min_y = min(y1, y2)
                 max_y = max(y1, y2)
                 for y in range(min_y, max_y + 1):
-                    sand_map[x1, y] = "#"
+                    sand_map[x1, y] = ROCK
             elif y1 == y2:
                 min_x = min(x1, x2)
                 max_x = max(x1, x2)
                 for x in range(min_x, max_x + 1):
-                    sand_map[x, y1] = "#"
+                    sand_map[x, y1] = ROCK
             else:
                 raise ValueError("Uh oh!")
 
     lowest_rock_y = max(y for _, y in sand_map.keys())
+    if fall_to_floor:
+        lowest_rock_y += 2
 
     sand_x, sand_y = SAND_SOURCE
-    while sand_y <= lowest_rock_y:
-        if (sand_x, sand_y + 1) not in sand_map:
+    while sand_y <= lowest_rock_y and sand_map[SAND_SOURCE] != SAND:
+        if fall_to_floor and sand_y == lowest_rock_y - 1:
+            # Right above the floor, can't fall lower
+            sand_map[sand_x, sand_y] = SAND
+            sand_x, sand_y = SAND_SOURCE
+        elif (sand_x, sand_y + 1) not in sand_map:
             sand_y += 1
         elif (sand_x - 1, sand_y + 1) not in sand_map:
             sand_x -= 1
@@ -63,11 +72,11 @@ def num_resting_units(puzzle_input, fall_to_floor=False):
             sand_x += 1
             sand_y += 1
         else:
-            sand_map[sand_x, sand_y] = "o"
+            sand_map[sand_x, sand_y] = SAND
             sand_x, sand_y = SAND_SOURCE
 
     # If the sand drops below the lowest rock, it will fall forever
-    return sum(obj == "o" for obj in sand_map.values())
+    return sum(obj == SAND for obj in sand_map.values())
 
 
 if __name__ == "__main__":
