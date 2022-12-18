bf17b1eef235902b16a65077f890d04bb59bf798
Michael Jalkio <mjalkio@gmail.com>
2022-12-18 13:36:07 -0800

    Parse inputs

--- year_2022/day15/beacon_exclusion_zone.py
+++ year_2022/day15/beacon_exclusion_zone.py
@@ -1,7 +1,20 @@
 from util import read_puzzle_input
 
 
+def manhattan_distance(point_a, point_b):
+    return sum(abs(a - b) for a, b in zip(point_a, point_b))
+
+
 def num_positions_cannot_contain_beacon(puzzle_input, row_num=2_000_000):
+    positions_cannot_contain_beacon = set()
+    for line in puzzle_input.split("\n"):
+        if line == "":
+            continue
+        sensor, beacon = line.split(": ")
+        sensor_x = int(sensor[sensor.find("=") + 1 : sensor.find(",")])
+        sensor_y = int(sensor[sensor.rfind("=") + 1 :])
+        beacon_x = int(beacon[beacon.find("=") + 1 : beacon.find(",")])
+        beacon_y = int(beacon[beacon.rfind("=") + 1 :])
     return 0
 
 
