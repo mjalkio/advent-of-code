71cad898679efea0fb8eb5535f52d3e822d06be6
Michael Jalkio <mjalkio@gmail.com>
2022-12-19 09:55:10 -0800

    Solve day 18 part 1

--- year_2022/day18/boiling_boulders.py
+++ year_2022/day18/boiling_boulders.py
@@ -2,7 +2,24 @@ from util import read_puzzle_input
 
 
 def approximate_surface_area(puzzle_input):
-    return 0
+    cubes = set()
+    for line in puzzle_input.strip().split("\n"):
+        x, y, z = [int(num) for num in line.split(",")]
+        cubes.add((x, y, z))
+
+    approx_surface_area = 0
+    for x, y, z in cubes:
+        for adjacent_cube in [
+            (x + 1, y, z),
+            (x - 1, y, z),
+            (x, y + 1, z),
+            (x, y - 1, z),
+            (x, y, z + 1),
+            (x, y, z - 1),
+        ]:
+            if adjacent_cube not in cubes:
+                approx_surface_area += 1
+    return approx_surface_area
 
 
 if __name__ == "__main__":
