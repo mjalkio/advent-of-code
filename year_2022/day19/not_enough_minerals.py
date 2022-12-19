2f63cce21e158967148e12dab9e3eee2a170fdf1
Michael Jalkio <mjalkio@gmail.com>
2022-12-19 12:31:50 -0800

    Parse inputs

--- year_2022/day19/not_enough_minerals.py
+++ year_2022/day19/not_enough_minerals.py
@@ -1,7 +1,24 @@
+from collections import namedtuple
+
 from util import read_puzzle_input
 
 
+Minerals = namedtuple("Minerals", ["ore", "clay", "obsidian"], defaults=[0, 0, 0])
+
+
 def sum_quality_levels(puzzle_input):
+    # Goal is to produce geodes
+    #
+    # Geode robots require ore and obsidian
+    # Obsidian robots require ore and clay
+    # Clay robots require ore
+    # Ore robots require ore
+    for blueprint in puzzle_input.strip().split("\n"):
+        words = blueprint.split()
+        ore_robot_cost = Minerals(ore=int(words[6]))
+        clay_robot_cost = Minerals(ore=int(words[12]))
+        obsidian_robot_cost = Minerals(ore=int(words[18]), clay=int(words[21]))
+        geode_robot_cost = Minerals(ore=int(words[27]), obsidian=int(words[30]))
     return 0
 
 
