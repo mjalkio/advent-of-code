65a2642857140e18a0639e5f90fe610f2926c744
Michael Jalkio <mjalkio@gmail.com>
2022-12-18 18:48:39 -0800

    Mark test as slow

--- year_2022/day16/test_proboscidea_volcanium.py
+++ year_2022/day16/test_proboscidea_volcanium.py
@@ -1,3 +1,5 @@
+import pytest
+
 from util import read_puzzle_input
 from year_2022.day16.proboscidea_volcanium import (
     most_pressure_possible,
@@ -9,6 +11,7 @@ def test_most_pressure_possible():
     assert most_pressure_possible(read_puzzle_input("test_input.txt")) == 1651
 
 
+@pytest.mark.slow
 def test_most_pressure_possible_with_elephant():
     assert (
         most_pressure_possible_with_elephant(read_puzzle_input("test_input.txt"))
