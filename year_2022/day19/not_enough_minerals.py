dee5672a363b994c1412dbebaa771e0584b981fc
Michael Jalkio <mjalkio@gmail.com>
2022-12-19 16:08:09 -0800

    Refactor day 19

--- year_2022/day19/not_enough_minerals.py
+++ year_2022/day19/not_enough_minerals.py
@@ -10,7 +10,7 @@ GEODE_IDX = 3
 State = namedtuple("State", ["minute", "ore_counts", "robot_counts"])
 
 
-TIME_LIMIT = 24
+TIME_LIMIT_PART_1 = 24
 
 
 def _add(a, b):
@@ -31,7 +31,7 @@ def _greater_than(a, b):
     return any(a[i] > b[i] for i in range(len(a)))
 
 
-def _next_states(state, robot_costs):
+def _next_states(state, robot_costs, time_limit):
     minute, ore_counts, robot_counts = state
     next_states = []
 
@@ -50,13 +50,13 @@ def _next_states(state, robot_costs):
         # We need to have enough time
         new_minute = minute
         new_ore_counts = ore_counts
-        while new_minute < TIME_LIMIT and _greater_than(cost, new_ore_counts):
+        while new_minute < time_limit and _greater_than(cost, new_ore_counts):
             # TODO: Can be sped up
             # calculate number of steps needed instead of going one at a time
             new_minute += 1
             new_ore_counts = _add(new_ore_counts, robot_counts)
 
-        if new_minute != TIME_LIMIT:
+        if new_minute != time_limit:
             new_ore_counts = _sub(new_ore_counts, cost)
             new_ore_counts = _add(new_ore_counts, robot_counts)
             new_robot_counts = _increment(robot_counts, robot_idx)
@@ -71,38 +71,42 @@ def _next_states(state, robot_costs):
     return next_states
 
 
+def _get_most_geodes_produced(blueprint, time_limit):
+    most_geodes_produced = 0
+    words = blueprint.split()
+    ore_robot_cost = Minerals(ore=int(words[6]))
+    clay_robot_cost = Minerals(ore=int(words[12]))
+    obsidian_robot_cost = Minerals(ore=int(words[18]), clay=int(words[21]))
+    geode_robot_cost = Minerals(ore=int(words[27]), obsidian=int(words[30]))
+    robot_costs = Minerals(
+        ore=ore_robot_cost,
+        clay=clay_robot_cost,
+        obsidian=obsidian_robot_cost,
+        geode=geode_robot_cost,
+    )
+
+    stack = [State(minute=0, ore_counts=Minerals(), robot_counts=Minerals(ore=1))]
+    while len(stack) > 0:
+        state = stack.pop()
+        next_states = _next_states(state, robot_costs, time_limit)
+
+        if len(next_states) == 0:
+            # We can't build more robots from this state
+            geodes_produced = state.ore_counts.geode
+            geodes_produced += state.robot_counts.geode * (time_limit - state.minute)
+            most_geodes_produced = max(geodes_produced, most_geodes_produced)
+        stack += next_states
+    return most_geodes_produced
+
+
 def sum_quality_levels(puzzle_input):
     quality_level_sum = 0
 
-    for blueprint in puzzle_input.strip().split("\n"):
-        most_geodes_produced = 0
-        words = blueprint.split()
-        blueprint_idx = int(words[1][:-1])
-        ore_robot_cost = Minerals(ore=int(words[6]))
-        clay_robot_cost = Minerals(ore=int(words[12]))
-        obsidian_robot_cost = Minerals(ore=int(words[18]), clay=int(words[21]))
-        geode_robot_cost = Minerals(ore=int(words[27]), obsidian=int(words[30]))
-        robot_costs = Minerals(
-            ore=ore_robot_cost,
-            clay=clay_robot_cost,
-            obsidian=obsidian_robot_cost,
-            geode=geode_robot_cost,
+    for i, blueprint in enumerate(puzzle_input.strip().split("\n")):
+        most_geodes_produced = _get_most_geodes_produced(
+            blueprint, time_limit=TIME_LIMIT_PART_1
         )
-
-        stack = [State(minute=0, ore_counts=Minerals(), robot_counts=Minerals(ore=1))]
-        while len(stack) > 0:
-            state = stack.pop()
-            next_states = _next_states(state, robot_costs)
-
-            if len(next_states) == 0:
-                # We can't build more robots from this state
-                geodes_produced = state.ore_counts.geode
-                geodes_produced += state.robot_counts.geode * (
-                    TIME_LIMIT - state.minute
-                )
-                most_geodes_produced = max(geodes_produced, most_geodes_produced)
-            stack += next_states
-        quality_level_sum += blueprint_idx * most_geodes_produced
+        quality_level_sum += (i + 1) * most_geodes_produced
     return quality_level_sum
 
 
