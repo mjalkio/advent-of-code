e862965e00420c42cf889f22ae48437e1e5eacae
Michael Jalkio <mjalkio@gmail.com>
2022-12-19 16:35:35 -0800

    Mark day 19 tests as slow

--- year_2022/day19/test_not_enough_minerals.py
+++ year_2022/day19/test_not_enough_minerals.py
@@ -1,3 +1,5 @@
+import pytest
+
 from util import read_puzzle_input
 from year_2022.day19.not_enough_minerals import (
     geode_count_product,
@@ -5,9 +7,11 @@ from year_2022.day19.not_enough_minerals import (
 )
 
 
+@pytest.mark.slow
 def test_sum_quality_levels():
     assert sum_quality_levels(read_puzzle_input("test_input.txt")) == 33
 
 
+@pytest.mark.slow
 def test_geode_count_product():
     assert geode_count_product(read_puzzle_input("test_input.txt")) == 56 * 62
