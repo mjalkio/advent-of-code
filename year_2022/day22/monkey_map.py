d7ccb9e3812bf52410b07af87cf493970eecd866
Michael Jalkio <mjalkio@gmail.com>
2022-12-22 09:20:09 -0500

    Parse input

--- year_2022/day22/monkey_map.py
+++ year_2022/day22/monkey_map.py
@@ -1,7 +1,35 @@
 from util import read_puzzle_input
 
+RIGHT = "R"
+LEFT = "L"
+
+
+def _parse_input(puzzle_input):
+    board, path = puzzle_input.split("\n\n")
+    board_map = {}
+    for y, line in enumerate(board.split("\n")):
+        for x in range(len(line)):
+            if line[x] != " ":
+                board_map[x + 1, y + 1] = line[x]
+
+    path_lst = []
+    while len(path) > 0:
+        if LEFT not in path and RIGHT not in path:
+            path_lst.append(int(path))
+            path = ""
+        else:
+            for i in range(len(path)):
+                if path[i] in (LEFT, RIGHT):
+                    path_lst.append(int(path[:i]))
+                    path_lst.append(path[i])
+                    path = path[i + 1 :]
+                    break
+
+    return board_map, path_lst
+
 
 def final_password(puzzle_input):
+    board, path = _parse_input(puzzle_input)
     return 0
 
 
