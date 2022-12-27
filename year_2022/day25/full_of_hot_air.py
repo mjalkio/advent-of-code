895ef21be9aa3b98d22ef8683fd2ce685571109d
Michael Jalkio <mjalkio@gmail.com>
2022-12-27 18:56:22 -0500

    Implement decimal_to_snafu

--- year_2022/day25/full_of_hot_air.py
+++ year_2022/day25/full_of_hot_air.py
@@ -26,37 +26,35 @@ def decimal_to_snafu(number):
     num_characters = 1
     max_number = 2
     while max_number < number:
+        max_number += 5 ** num_characters * 2
         num_characters += 1
-        max_number += 5 ** num_characters
     snafu_number = ""
     current_decimal = 0
     for i in reversed(range(num_characters)):
-        character_found = False
-        amount_could_subtract = sum(5 ** j * -2 for j in range(i))
-        amount_could_add = sum(5 ** j * 2 for j in range(i))
+        remaining_magnitude = sum(5 ** j * 2 for j in range(i))
 
-        for snafu_char in ("=", "-"):
-            if (
-                current_decimal + (5 ** i * SNAFU_MAP[snafu_char]) + amount_could_add
-                >= number
-                and not character_found
-            ):
-                snafu_number += snafu_char
-                character_found = True
+        if current_decimal + remaining_magnitude < number:
+            if current_decimal + remaining_magnitude + 5 ** i >= number:
+                snafu_number += "1"
+                current_decimal += 5 ** i
+                continue
+            else:
+                snafu_number += "2"
+                current_decimal += 5 ** i * 2
+                continue
 
-        for snafu_char in ("2", "1"):
-            if (
-                current_decimal
-                + (5 ** i * SNAFU_MAP[snafu_char])
-                - amount_could_subtract
-                <= number
-                and not character_found
-            ):
-                snafu_number += snafu_char
-                character_found = True
+        if current_decimal - remaining_magnitude > number:
+            if current_decimal - remaining_magnitude - 5 ** i <= number:
+                snafu_number += "-"
+                current_decimal -= 5 ** i
+                continue
+            else:
+                snafu_number += "="
+                current_decimal -= 5 ** i * 2
+                continue
 
-        if not character_found:
-            snafu_number += "0"
+        snafu_number += "0"
+
     return snafu_number
 
 
