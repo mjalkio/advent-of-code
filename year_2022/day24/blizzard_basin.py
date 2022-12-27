0a5cef6f5f6db0609576a2fd4b723e60ca8966e5
Michael Jalkio <mjalkio@gmail.com>
2022-12-27 14:24:03 -0500

    Parse input

--- year_2022/day24/blizzard_basin.py
+++ year_2022/day24/blizzard_basin.py
@@ -1,7 +1,36 @@
 from util import read_puzzle_input
 
+STARTING_POS = (1, 0)
+GOAL_POS = (6, -5)
+
+
+def _parse_input(puzzle_input):
+    blizzards = {}
+    for y, line in enumerate(puzzle_input.strip().split("\n")):
+        for x in range(len(line)):
+            if line[x] != ".":
+                blizzards[x, -y] = line[x]
+    return blizzards
+
+
+def _print(blizzards):
+    min_x = min(x for x, y in blizzards)
+    min_y = min(y for x, y in blizzards)
+    max_x = max(x for x, y in blizzards)
+    max_y = max(y for x, y in blizzards)
+
+    for y in reversed(range(min_y, max_y + 1)):
+        line = ""
+        for x in range(min_x, max_x + 1):
+            if (x, y) not in blizzards:
+                line += "."
+            else:
+                line += blizzards[x, y]
+        print(line)
+
 
 def num_minutes_to_goal(puzzle_input):
+    blizzards = _parse_input(puzzle_input)
     return 0
 
 
