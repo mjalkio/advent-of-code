33f7f1affc186180bd58d179f3a667d1603f59cf
Michael Jalkio <mjalkio@gmail.com>
2022-12-27 17:44:46 -0500

    Implement sum_fuel_requirements

--- year_2022/day25/full_of_hot_air.py
+++ year_2022/day25/full_of_hot_air.py
@@ -2,7 +2,8 @@ from util import read_puzzle_input
 
 
 def sum_fuel_requirements(puzzle_input):
-    return 0
+    snafu_nums = puzzle_input.strip().split("\n")
+    return sum(snafu_to_decimal(num) for num in snafu_nums)
 
 
 def snafu_to_decimal(number):
