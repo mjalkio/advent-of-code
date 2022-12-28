b7db55c1b2417281abebc3668b66af56456c2832
Michael Jalkio <mjalkio@gmail.com>
2022-12-27 21:30:27 -0800

    Fix formatting with latest version of Black

--- year_2022/day25/full_of_hot_air.py
+++ year_2022/day25/full_of_hot_air.py
@@ -18,7 +18,7 @@ def sum_fuel_requirements(puzzle_input):
 def snafu_to_decimal(number):
     decimal_number = 0
     for i, char in enumerate(reversed(number)):
-        decimal_number += 5 ** i * SNAFU_MAP[char]
+        decimal_number += 5**i * SNAFU_MAP[char]
     return decimal_number
 
 
@@ -26,31 +26,31 @@ def decimal_to_snafu(number):
     num_characters = 1
     max_number = 2
     while max_number < number:
-        max_number += 5 ** num_characters * 2
+        max_number += 5**num_characters * 2
         num_characters += 1
     snafu_number = ""
     current_decimal = 0
     for i in reversed(range(num_characters)):
-        remaining_magnitude = sum(5 ** j * 2 for j in range(i))
+        remaining_magnitude = sum(5**j * 2 for j in range(i))
 
         if current_decimal + remaining_magnitude < number:
-            if current_decimal + remaining_magnitude + 5 ** i >= number:
+            if current_decimal + remaining_magnitude + 5**i >= number:
                 snafu_number += "1"
-                current_decimal += 5 ** i
+                current_decimal += 5**i
                 continue
             else:
                 snafu_number += "2"
-                current_decimal += 5 ** i * 2
+                current_decimal += 5**i * 2
                 continue
 
         if current_decimal - remaining_magnitude > number:
-            if current_decimal - remaining_magnitude - 5 ** i <= number:
+            if current_decimal - remaining_magnitude - 5**i <= number:
                 snafu_number += "-"
-                current_decimal -= 5 ** i
+                current_decimal -= 5**i
                 continue
             else:
                 snafu_number += "="
-                current_decimal -= 5 ** i * 2
+                current_decimal -= 5**i * 2
                 continue
 
         snafu_number += "0"
