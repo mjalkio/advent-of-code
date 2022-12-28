076e4ad2bc556b42237cee4cb337d98412e8cd8d
Michael Jalkio <mjalkio@gmail.com>
2022-12-27 21:25:35 -0800

    Finish Advent of Code 2022

--- year_2022/day25/full_of_hot_air.py
+++ year_2022/day25/full_of_hot_air.py
@@ -62,4 +62,3 @@ if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {decimal_to_snafu(sum_fuel_requirements(puzzle_input))}")
-    print(f"Part 2: {sum_fuel_requirements(puzzle_input)}")
