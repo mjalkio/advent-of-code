1b915c1e78944281daa6321583a5d49853fe0308
Michael Jalkio <mjalkio@gmail.com>
2023-12-02 18:07:15 +0000

    Solve day 2 part 1

--- year_2023/day02/cube_conundrum.py
+++ year_2023/day02/cube_conundrum.py
@@ -1,8 +1,28 @@
 from util import read_puzzle_input
 
 
-def sum_possible_games(puzzle_input):
-    return 0
+def _is_possible(subset, bag):
+    cube_counts = subset.split(", ")
+    for count in cube_counts:
+        num_cubes, cube_color = count.split(" ")
+        num_cubes = int(num_cubes)
+        if bag[cube_color] < num_cubes:
+            return False
+
+    return True
+
+
+def sum_possible_games(puzzle_input, bag={"red": 12, "green": 13, "blue": 14}):
+    answer = 0
+    games = puzzle_input.split("\n")
+    for game in games:
+        game_part, subset_part = game.split(": ")
+        game_id = int(game_part[5:])
+        subsets = subset_part.split("; ")
+        if all(_is_possible(subset, bag) for subset in subsets):
+            answer += game_id
+
+    return answer
 
 
 if __name__ == "__main__":
