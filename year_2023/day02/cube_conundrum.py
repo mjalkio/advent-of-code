06981f0501dcbb522fa534d9169593231eba3d84
Michael Jalkio <mjalkio@gmail.com>
2023-12-02 18:14:37 +0000

    Solve day 2 part 2

--- year_2023/day02/cube_conundrum.py
+++ year_2023/day02/cube_conundrum.py
@@ -1,3 +1,5 @@
+import math
+
 from util import read_puzzle_input
 
 
@@ -27,6 +29,19 @@ def sum_possible_games(puzzle_input, bag={"red": 12, "green": 13, "blue": 14}):
 
 def sum_min_set_powers(puzzle_input):
     answer = 0
+    games = puzzle_input.split("\n")
+    for game in games:
+        _, subset_part = game.split(": ")
+        subsets = subset_part.split("; ")
+        min_set = {"red": 0, "green": 0, "blue": 0}
+        for subset in subsets:
+            cube_counts = subset.split(", ")
+            for count in cube_counts:
+                num_cubes, cube_color = count.split(" ")
+                num_cubes = int(num_cubes)
+                if min_set[cube_color] < num_cubes:
+                    min_set[cube_color] = num_cubes
+        answer += math.prod(min_set.values())
     return answer
 
 
