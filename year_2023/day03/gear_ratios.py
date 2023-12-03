a30a5867518b6583a23ed15065c8765a6c812ebb
Michael Jalkio <mjalkio@gmail.com>
2023-12-03 05:31:33 +0000

    Add part 2 outline

--- year_2023/day03/gear_ratios.py
+++ year_2023/day03/gear_ratios.py
@@ -50,8 +50,12 @@ def sum_part_numbers(puzzle_input):
     return answer
 
 
+def sum_gear_ratios(puzzle_input):
+    return 0
+
+
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {sum_part_numbers(puzzle_input)}")
-    print(f"Part 2: {sum_part_numbers(puzzle_input)}")
+    print(f"Part 2: {sum_gear_ratios(puzzle_input)}")
--- year_2023/day03/test_gear_ratios.py
+++ year_2023/day03/test_gear_ratios.py
@@ -1,8 +1,13 @@
 from util import read_puzzle_input
 from year_2023.day03.gear_ratios import (
+    sum_gear_ratios,
     sum_part_numbers,
 )
 
 
 def test_sum_part_numbers():
     assert sum_part_numbers(read_puzzle_input("test_input.txt")) == 4361
+
+
+def test_sum_gear_ratios():
+    assert sum_gear_ratios(read_puzzle_input("test_input.txt")) == 467835
