74ce07952adde7b6e3564b3103b1d90f89fbcd88
Michael Jalkio <mjalkio@gmail.com>
2023-12-25 22:30:28 +0000

    Add part 2 outline

--- year_2023/day06/test_wait_for_it.py
+++ year_2023/day06/test_wait_for_it.py
@@ -1,8 +1,13 @@
 from util import read_puzzle_input
 from year_2023.day06.wait_for_it import (
+    get_num_ways_to_beat_record,
     get_product_num_ways_beat_record,
 )
 
 
 def test_get_product_num_ways_beat_record():
     assert get_product_num_ways_beat_record(read_puzzle_input("test_input.txt")) == 288
+
+
+def test_get_num_ways_to_beat_record():
+    assert get_num_ways_to_beat_record(read_puzzle_input("test_input.txt")) == 71503
--- year_2023/day06/wait_for_it.py
+++ year_2023/day06/wait_for_it.py
@@ -21,8 +21,12 @@ def get_product_num_ways_beat_record(puzzle_input):
     return math.prod(ways_to_beat_races)
 
 
+def get_num_ways_to_beat_record(puzzle_input):
+    return 0
+
+
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
 
     print(f"Part 1: {get_product_num_ways_beat_record(puzzle_input)}")
-    print(f"Part 2: {get_product_num_ways_beat_record(puzzle_input)}")
+    print(f"Part 2: {get_num_ways_to_beat_record(puzzle_input)}")
