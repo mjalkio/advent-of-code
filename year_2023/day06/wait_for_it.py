3ca6b1c5dd06276b059146598af3806e85569e74
Michael Jalkio <mjalkio@gmail.com>
2023-12-25 22:27:57 +0000

    Solve day 6 part 1

--- year_2023/day06/wait_for_it.py
+++ year_2023/day06/wait_for_it.py
@@ -1,8 +1,24 @@
+import math
+
 from util import read_puzzle_input
 
 
 def get_product_num_ways_beat_record(puzzle_input):
-    return 0
+    time_input, distance_input = puzzle_input.split("\n")
+    times = [int(t) for t in time_input.split("Time:")[1].split(" ") if t != ""]
+    records = [
+        int(d) for d in distance_input.split("Distance:")[1].split(" ") if d != ""
+    ]
+    races = zip(times, records)
+    ways_to_beat_races = []
+    for time, record in races:
+        num_ways = 0
+        for button_hold in range(1, time):
+            distance = button_hold * (time - button_hold)
+            if distance > record:
+                num_ways += 1
+        ways_to_beat_races.append(num_ways)
+    return math.prod(ways_to_beat_races)
 
 
 if __name__ == "__main__":
