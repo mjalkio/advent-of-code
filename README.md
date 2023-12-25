467ee416449b922b4b840412c4d35dc48afe0d69
Michael Jalkio <mjalkio@gmail.com>
2023-12-25 02:36:15 +0000

    Solve day 4 part 1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Repository to hold Advent of Code (http://adventofcode.com/) solutions.
 * Set up a virtual environment `python -m venv venv`
 * Activate it `source venv/bin/activate`
 * Install requirements `pip install -r requirements.txt`
-* Set up PYTHONPATH `export PYTHONPATH="${PYTHONPATH}:/path/to/advent-of-code"`
+* Set up PYTHONPATH `export PYTHONPATH="${PYTHONPATH}:/path/to/advent-of-code"` eg `export PYTHONPATH=/workspaces/advent-of-code`
 * Use `python util.py` to set up each day
 * Run each day using `python year_2023/day01/trebuchet.py`
 * Update tox.ini to set default year for testing
--- year_2023/day04/scratchcards.py
+++ year_2023/day04/scratchcards.py
@@ -1,8 +1,26 @@
 from util import read_puzzle_input
 
 
+def _split_numbers(number_input):
+    # Numbers are all between 1 and 99
+    return [number_input[i : i + 3].strip() for i in range(0, len(number_input), 3)]
+
+
 def get_point_value(puzzle_input):
-    return 0
+    point_value = 0
+    card_inputs = puzzle_input.split("\n")
+    for card in card_inputs:
+        winning_input, number_input = card.split(": ")[1].split(" | ")
+        winning_numbers = _split_numbers(winning_input)
+        numbers_you_have = _split_numbers(number_input)
+        winners = [num for num in numbers_you_have if num in winning_numbers]
+        num_wins = len(winners)
+        if num_wins == 1:
+            point_value += 1
+        elif num_wins > 1:
+            point_value += 2 ** (num_wins - 1)
+        # print(f"Num wins {num_wins}, point_value {point_value}")
+    return point_value
 
 
 if __name__ == "__main__":
