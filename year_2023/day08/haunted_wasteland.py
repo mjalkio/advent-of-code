9ea01a3c7b333c8107dfa654bdcb8c76977501c1
Michael Jalkio <mjalkio@gmail.com>
2023-12-27 04:03:36 +0000

    Solve day 8 part 1

--- year_2023/day08/haunted_wasteland.py
+++ year_2023/day08/haunted_wasteland.py
@@ -1,8 +1,30 @@
 from util import read_puzzle_input
 
 
+LEFT = "L"
+RIGHT = "R"
+
+END = "ZZZ"
+START = "AAA"
+
+
 def get_num_steps(puzzle_input):
-    return 0
+    lines = puzzle_input.split("\n")
+    instructions = lines[0]
+    node_inputs = lines[2:]
+    nodes = {}
+    for ni in node_inputs:
+        node, connections = ni.split(" = ")
+        left, right = connections[1:-1].split(", ")
+        nodes[node] = {LEFT: left, RIGHT: right}
+
+    current_node = START
+    num_steps = 0
+    while current_node != END:
+        next_instruction = instructions[num_steps % len(instructions)]
+        current_node = nodes[current_node][next_instruction]
+        num_steps += 1
+    return num_steps
 
 
 if __name__ == "__main__":
