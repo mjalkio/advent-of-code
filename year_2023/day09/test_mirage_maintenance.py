62827296d030815e4a097e372237579be336bb09
Michael Jalkio <mjalkio@gmail.com>
2023-12-28 02:19:59 +0000

    Add day 9 outline

---
+++ year_2023/day09/test_mirage_maintenance.py
@@ -0,0 +1,8 @@
+from util import read_puzzle_input
+from year_2023.day09.mirage_maintenance import (
+    sum_extrapolated_values,
+)
+
+
+def test_sum_extrapolated_values():
+    assert sum_extrapolated_values(read_puzzle_input("test_input.txt")) == 114
