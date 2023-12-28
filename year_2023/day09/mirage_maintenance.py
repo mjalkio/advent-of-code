431aa8bc42a09fd2dd09fedd38cf0abd4284088d
Michael Jalkio <mjalkio@gmail.com>
2023-12-28 02:53:18 +0000

    Solve day 9 part 1

--- year_2023/day09/mirage_maintenance.py
+++ year_2023/day09/mirage_maintenance.py
@@ -2,7 +2,22 @@ from util import read_puzzle_input
 
 
 def sum_extrapolated_values(puzzle_input):
-    return 0
+    histories = puzzle_input.split("\n")
+    extrapolated_sum = 0
+    for hist in histories:
+        hist = [int(val) for val in hist.split(" ")]
+        sequences = [hist]
+        while any(num != 0 for num in sequences[-1]):
+            values = sequences[-1]
+            differences = [values[i + 1] - values[i] for i in range(len(values) - 1)]
+            sequences.append(differences)
+
+        sequences[-1].append(0)
+        for i in reversed(range(len(sequences) - 1)):
+            sequences[i].append(sequences[i][-1] + sequences[i + 1][-1])
+        
+        extrapolated_sum += sequences[0][-1]
+    return extrapolated_sum
 
 
 if __name__ == "__main__":
